9624db121226be24924b30a10b805347bee4a916
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-11-28 17:16:21 +0000

    214. Crear modelos

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/3.1/ref/models/fields/
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content =   models.TextField()
+    public = models.BooleanField()
+    created_at = models.models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description =   models.CharField(max_length=250)
+    created_at = models.DateField()
+    
