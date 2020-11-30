4c21b164c7d7fb51d4204f61735d343a40c4011a
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-11-30 00:04:48 +0000

    Correcciones codigo modelo

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    content =   models.TextField()
+    content = models.TextField()
     public = models.BooleanField()
-    created_at = models.models.DateTimeField(auto_now=False, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description =   models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
     created_at = models.DateField()
     
