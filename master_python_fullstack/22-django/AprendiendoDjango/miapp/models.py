37185217d13fb511357609bd1778983d0df5b92b
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-11-30 00:17:54 +0000

    216. Hacer cambios en la estructura de tablas y modelos

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
@@ -3,14 +3,15 @@ from django.db import models
 # Create your models here.
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     content = models.TextField()
+    image = models.ImageField(default='null')
     public = models.BooleanField()
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=110)
     description = models.CharField(max_length=250)
     created_at = models.DateField()
     
