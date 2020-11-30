35c7cc1d9d77178052e95df474931e97152cb8bb
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-11-30 23:07:03 +0000

    236. Personalizar formularios

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/forms.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/forms.py
@@ -3,13 +3,27 @@ from django import forms
 class FormArticle(forms.Form):
 
     title = forms.CharField(
-        label = 'Título'
+        label = 'Título',
+        max_length=40,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Introduzca el título',
+                'class': 'titulo_form_article'
+            }
+        )
     )
 
     content = forms.CharField(
         label = 'Contenido',
-        widget = forms.Textarea
+        widget=forms.Textarea
     )
+    content.widget.attrs.update({
+        'placeholder': 'Contenido',
+        'class': 'contenido_form_article',
+        'id': 'contenido_form'
+    })
+
 
     public_options = [
         (1, 'Si'),
