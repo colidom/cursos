d6b9f27f0802a7284769dbcca6d8b25866645135
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-11-30 22:14:06 +0000

    234. Campo de tipo Select

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/forms.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/forms.py
@@ -5,7 +5,18 @@ class FormArticle(forms.Form):
     title = forms.CharField(
         label = 'Título'
     )
+
     content = forms.CharField(
         label = 'Contenido',
-        widget = forms.Textarea()
+        widget = forms.Textarea
+    )
+
+    public_options = [
+        (1, 'Si'),
+        (0, 'No')
+    ]
+
+    select = forms.TypedChoiceField(
+        label = "¿Publicado?",
+        choices = public_options
     )
