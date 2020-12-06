c70a6146872c6571764885e7e13cd861743d0556
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-06 00:26:34 +0000

    240. Clase Meta de los modelos de Django 3

--- master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
+++ master_python_fullstack/22-django/AprendiendoDjango/miapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/
+# https://docs.djangoproject.com/en/3.1/ref/models/options/
 class Article(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
@@ -9,9 +10,17 @@ class Article(models.Model):
     public = models.BooleanField()
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos" 
+        ordering = ['-created_at']
 
 class Category(models.Model):
     name = models.CharField(max_length=110)
     description = models.CharField(max_length=250)
     created_at = models.DateField()
     
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías" 
