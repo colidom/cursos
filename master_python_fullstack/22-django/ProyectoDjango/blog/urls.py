daef000e3093b1af0ab846b57870566f19336e04
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 23:33:43 +0000

    284. Una página para cada artículo

--- master_python_fullstack/22-django/ProyectoDjango/blog/urls.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('articulos/', views.list, name='list_articles'),
     path('categoria/<int:category_id>', views.category, name='category'),
+    path('articulo/<int:article_id>', views.article, name='article'),
 ]
--- master_python_fullstack/22-django/ProyectoDjango/blog/views.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/views.py
@@ -20,3 +20,11 @@ def category(request, category_id):
         'category': category,
         'articles': articles
     })
+
+def article(request, article_id):
+
+    article = get_object_or_404(Article, id=article_id)
+
+    return render(request, 'articles/detail.html', {
+        'article': article
+    })
