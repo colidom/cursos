06e994b0b11fce869a3f22cb55d1b4ef700f0c69
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 01:37:07 +0000

    256. Modelo para la entidad Página

--- master_python_fullstack/22-django/ProyectoDjango/pages/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Título")
+    contentent = models.TextField(verbose_name="Contenido")
+    slug = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
+    visible = models.BooleanField(verbose_name="¿Visible?")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Modificado el")
+
+    class Meta:
+        verbose_name = "Página"
+        verbose_name_plural = "Páginas"
+        
+    def __str__(self):
+        return self.title
