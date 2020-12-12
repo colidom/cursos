a5e5dced571ffb5027f88cd2b44c8f7de2096d1b
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 02:45:50 +0000

    265. Usar un editor de texto enriquecido en Django

--- master_python_fullstack/22-django/ProyectoDjango/pages/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name="Título")
-    content = models.TextField(verbose_name="Contenido")
+    content = RichTextField(verbose_name="Contenido")
     slug = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
     visible = models.BooleanField(verbose_name="¿Visible?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
