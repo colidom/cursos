62d212bb99a173c2662efba9e663a93257064fb9
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 02:23:35 +0000

    Arreglando nombre de la valiable content dentro del modelo de la app pages + add migrations

--- master_python_fullstack/22-django/ProyectoDjango/pages/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name="Título")
-    contentent = models.TextField(verbose_name="Contenido")
+    content = models.TextField(verbose_name="Contenido")
     slug = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
     visible = models.BooleanField(verbose_name="¿Visible?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
