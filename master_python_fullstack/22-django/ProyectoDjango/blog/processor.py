32d18bc3cfd335f7834b2dfa898e4a5d88de585d
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 23:05:47 +0000

    280. Subconsultas en Django

--- master_python_fullstack/22-django/ProyectoDjango/blog/processor.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/processor.py
@@ -1,10 +1,12 @@
-from blog.models import Category
+from blog.models import Category, Article
 
 def get_categories(request):
 
-    categories = Category.objects.values_list('id', 'name')
+    caterogies_in_use = Article.objects.filter(public=True).values_list('categories', flat=True)
+    categories = Category.objects.filter(id__in=caterogies_in_use).values_list('id', 'name')
     
     return {
-        'categories': categories
+        'categories': categories,
+        'ids': caterogies_in_use
     }
     
