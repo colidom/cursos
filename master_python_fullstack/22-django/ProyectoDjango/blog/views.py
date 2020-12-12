56ad313fd9352312d8cea4818d02c3230ba2ad85
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 22:15:57 +0000

    275. La potencia de las relaciones

--- master_python_fullstack/22-django/ProyectoDjango/blog/views.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from blog.models import Category, Article
 
 # Create your views here.
 def list(request):
+
+    articles = Article.objects.all()
+
     return render(request, 'articles/list.html', {
-        'title': 'Artículos'
+        'title': 'Artículos',
+        'articles': articles
     })
