e3595df2850095a53ff5625602b798687fde68d9
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 23:25:16 +0000

    283. Datos relacionados entre modelos y relaciones inversas

--- master_python_fullstack/22-django/ProyectoDjango/blog/views.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/views.py
@@ -14,7 +14,9 @@ def list(request):
 def category(request, category_id):
 
     category = get_object_or_404(Category, id=category_id)
+    articles = Article.objects.filter(categories=category)
 
     return render(request, 'categories/category.html', {
-        'category': category
+        'category': category,
+        'articles': articles
     })
