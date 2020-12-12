8891a6010b8e48a5012fbff380bd0bcdc71349df
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 18:46:13 +0000

    270. Relaciones entre los modelos

--- master_python_fullstack/22-django/ProyectoDjango/blog/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -7,9 +9,25 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
 
     class Meta:
-        vervose_name = 'Categoría'
-        vervose_name_plural = 'Categorías'
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
     
     def __str__(self):
         return self.name
-        
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Título')
+    content = RichTextField(verbose_name='Contenido')
+    image = models.ImageField(default='null', verbose_name='Imágen')
+    public = models.BooleanField(verbose_name='¿Publicado')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuario')
+    categories = models.ManyToManyField(Category,  verbose_name='Categorías', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Editado el')
+
+    class Meta:
+    verbose_name = 'Artículo'
+    verbose_name_plural = 'Artículos'
+    
+    def __str__(self):
+        return self.name
