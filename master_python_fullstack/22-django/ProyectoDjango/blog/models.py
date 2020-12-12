330893c22977dca8da721a49c3ca0b313f8af2ef
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 18:32:43 +0000

    269. Crear modelo de categoría

--- master_python_fullstack/22-django/ProyectoDjango/blog/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    description = models.CharField(max_length=500, verbose_name='Descripción')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
+
+    class Meta:
+        vervose_name = 'Categoría'
+        vervose_name_plural = 'Categorías'
+    
+    def __str__(self):
+        return self.name
+        
