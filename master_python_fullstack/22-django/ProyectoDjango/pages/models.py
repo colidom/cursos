d7d934f59f33d1cc04a275b931c80c38552fc3b8
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 03:08:24 +0000

    267. Modificar el modelo y agregar orden en páginas

--- master_python_fullstack/22-django/ProyectoDjango/pages/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/models.py
@@ -6,6 +6,7 @@ class Page(models.Model):
     title = models.CharField(max_length=50, verbose_name="Título")
     content = RichTextField(verbose_name="Contenido")
     slug = models.CharField(unique=True, max_length=150, verbose_name="URL amigable")
+    order = models.IntegerField(default=0, verbose_name="Orden")
     visible = models.BooleanField(verbose_name="¿Visible?")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado el")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Modificado el")
