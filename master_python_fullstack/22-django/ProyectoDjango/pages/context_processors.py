c1b2ac66eeb70a5417746f49bc601cbd670dfcd5
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-12 02:26:50 +0000

    262. Solo mostrar páginas visibles

--- master_python_fullstack/22-django/ProyectoDjango/pages/context_processors.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/context_processors.py
@@ -2,7 +2,7 @@ from pages.models import Page
 
 def get_pages(request):
 
-    pages = Page.objects.values_list('id', 'title', 'slug')
+    pages = Page.objects.filter(visible=True).values_list('id', 'title', 'slug')
     
     return {
         'pages': pages
