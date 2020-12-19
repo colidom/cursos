6ee24433a8772516d6913185ef765b485ececab1
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-19 23:38:39 +0000

    286. Personalizar listados - Parte 1

--- master_python_fullstack/22-django/ProyectoDjango/blog/admin.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/admin.py
@@ -3,10 +3,14 @@ from .models import Category, Article
 
 class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at',)
+    list_display = ('name', 'created_at')
+    search_fields = ('name', 'description')
 
 class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ('user', 'created_at', 'updated_at')
-
+    search_fields = ('title', 'content', 'user__name', 'categories__name')
+    list_display = ('title', 'user', 'public', 'created_at')
+    list_filter = ('public', 'user__username', 'categories__name')
     def save_model(self, request, obj, form, change):
         if not obj.user_id:
             obj.user_id = request.user.id
