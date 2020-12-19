11f81b5578593c3890fdb92ea48d2146d83b95a3
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-19 23:20:26 +0000

    286. Personalizar listados - Parte 1

--- master_python_fullstack/22-django/ProyectoDjango/pages/admin.py
+++ master_python_fullstack/22-django/ProyectoDjango/pages/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Page
 
+# Configuración extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'visible', 'created_at')
+    ordering = ('created_at',)
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 title = "Proyecto con Django"
 subtitle = "Panel de gestión"
