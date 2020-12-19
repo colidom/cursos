65255277327b9cdb08880aeef6b6af6b11ec5b81
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-19 23:38:49 +0000

    Fix model blog

--- master_python_fullstack/22-django/ProyectoDjango/blog/models.py
+++ master_python_fullstack/22-django/ProyectoDjango/blog/models.py
@@ -19,7 +19,7 @@ class Article(models.Model):
     title = models.CharField(max_length=150, verbose_name='Título')
     content = RichTextField(verbose_name='Contenido')
     image = models.ImageField(default='null', verbose_name='Imágen', upload_to='articles')
-    public = models.BooleanField(verbose_name='¿Publicado')
+    public = models.BooleanField(verbose_name='¿Publicado?')
     user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE, verbose_name='Usuario')
     categories = models.ManyToManyField(Category,  verbose_name='Categorías', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
