2c4f00ca93625a76be46154d3cb3528d651a435c
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-21 00:03:40 +0000

    290. Formulario de registro  de usuarios

--- master_python_fullstack/22-django/ProyectoDjango/mainapp/views.py
+++ master_python_fullstack/22-django/ProyectoDjango/mainapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
@@ -13,6 +14,17 @@ def about(request):
 
 def register_page(request):
 
+    register_form = UserCreationForm()
+
+    if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+
+            return redirect('/inicio')
+
     return render(request, 'users/register.html', {
-        'title': 'Registro'
+        'title': 'Registro',
+        'register_form': register_form
     })
