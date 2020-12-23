fa3b119c044fb28addbe13233032c2460265b59d
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-23 18:27:30 +0000

    317. Insertar datos

--- master_python_fullstack/23-flask/main.py
+++ master_python_fullstack/23-flask/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'proyectoFlask'
+app.config['MYSQL_DB'] = 'proyectoflask'
 
 mysql = MySQL(app)
 
@@ -54,6 +54,14 @@ def contacto(redireccion = None):
 @app.route('/lenguajes-de-programacion')
 def lenguajes():
     return render_template('lenguajes.html')
+
+@app.route('/insertar-coche')
+def insertar_coche():
+    cursor = mysql.connection.cursor()
+    cursor.execute(f"INSERT INTO coches VALUES(NULL,'Lambo', 'Gallardo', '100.000', 'LA')")
+    cursor.connection.commit()
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
