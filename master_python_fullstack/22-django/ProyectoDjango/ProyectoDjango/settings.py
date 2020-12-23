49b3cc9ad5bb9c8d75f04589ae281a8127a9bca8
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-23 00:13:39 +0000

    297. Usar MySQL en Django

--- master_python_fullstack/22-django/ProyectoDjango/ProyectoDjango/settings.py
+++ master_python_fullstack/22-django/ProyectoDjango/ProyectoDjango/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'ProyectoDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+# https://www.lfd.uci.edu/~gohlke/pythonlibs/
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyectodjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
