e6c1b443f354179c1336e135c741f121a3937630
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-23 00:23:17 +0000

    299. Crear rutas

--- master_python_fullstack/23-flask/main.py
+++ master_python_fullstack/23-flask/main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return ("Aprendiendo Flask")
 
+@app.route('/información')
+def informacion():
+    return "<h1>Página de información </h1>"
+
+@app.route('/contacto')
+def contacto():
+    return "<h1>Página de contacto </h1>"
+
+@app.route('/lenguajes-de-programacion')
+def lenguajes():
+    return "<h1>Página de lenguajes </h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
