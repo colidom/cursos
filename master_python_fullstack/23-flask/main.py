8f5ed07063f75cbe3b89fd815ba14f17133578f1
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-23 16:22:02 +0000

    309. Pasar datos a la template y comentarios

--- master_python_fullstack/23-flask/main.py
+++ master_python_fullstack/23-flask/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', dato1='valor', dato2='valor2', lista= ["uno", "dos", "tres"])
 
 @app.route('/informacion')
 @app.route('/informacion/<string:nombre>')
