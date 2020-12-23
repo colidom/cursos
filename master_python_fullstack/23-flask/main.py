a8db67fe57543bc3d491da16f4684d88707a1ed1
Carlos Javier Oliva Domínguez <30834486+colidom@users.noreply.github.com>
2020-12-23 16:47:18 +0000

    311. Context processors y fechas en flask

--- master_python_fullstack/23-flask/main.py
+++ master_python_fullstack/23-flask/main.py
@@ -1,6 +1,14 @@
 from flask import Flask, redirect, url_for, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Context processors
+@app.context_processor
+def date_now():
+    return {
+        'now': datetime.utcnow()
+    }
 
 @app.route('/')
 def index():
