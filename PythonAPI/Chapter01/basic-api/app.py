9f031aa868351786f3c8e556f5518dec3b9ec4af
colidom <carlos.oliva.dom@gmail.com>
2021-01-21 21:19:22 +0000

    Añadidas funciones GET all recipes y GET recipes by id

--- PythonAPI/Chapter01/basic-api/app.py
+++ PythonAPI/Chapter01/basic-api/app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from http import HTTPStatus
 
 app = Flask(__name__)
+
+recipes = [
+    {
+        'id': 1,
+        'name': 'Egg Salad',
+        'description': 'This is a lovely egg salad recipe.'
+    },
+    {
+        'id': 2, 'name': 'Tomato Pasta',
+        'description': 'This is a lovely tomato pasta recipe.'
+    }
+]
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
-    
+
+
+@app.route('/recipes', methods=['GET'])
+def get_recipes():
+    return jsonify({'data': recipes})
+
+
+@app.route('/recipes/<int:recipe_id>', methods=['GET'])
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    if recipe:
+        return jsonify(recipe)
+    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
