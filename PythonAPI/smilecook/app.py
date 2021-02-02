fae56eae629a0779479c1a5d70f56429a0d4fddf
colidom <carlos.oliva.dom@gmail.com>
2021-02-02 22:25:50 +0000

    Manipulación de la DB con SQLALCHEMY. Definido modelos de la DB

--- PythonAPI/smilecook/app.py
+++ PythonAPI/smilecook/app.py
@@ -1,14 +1,32 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+from config import Config
+from extensions import db
+from models.user import User
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
-app = Flask(__name__)
-api = Api(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    register_extensions(app)
+    register_resources(app)
+    return app
 
 
-api.add_resource(RecipeListResource, '/recipes') 
-api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
-api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app = create_app()
+    app.run()
--- PythonAPI/smilecook/models/recipe.py
+++ PythonAPI/smilecook/models/recipe.py
@@ -1,7 +1,9 @@
+from extensions import db
+
 recipe_list = []
 
-
 def get_last_id():
+
     if recipe_list:
         last_recipe = recipe_list[-1]
     else:
@@ -9,25 +11,17 @@ def get_last_id():
     return last_recipe.id + 1
 
 
-class Recipe:
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
 
-    def __init__(self, name, description, num_of_servings, cook_time, directions):
-        self.id = get_last_id()
-        self.name = name
-        self.description = description
-        self.num_of_servings = num_of_servings
-        self.cook_time = cook_time
-        self.directions = directions
-        self.is_publish = False
-
-    @property
-    def data(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'num_of_servings': self.num_of_servings,
-            'cook_time': self.cook_time,
-            'directions': self.directions
-        }
-        
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    num_of_servings = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    directions = db.Column(db.String(1000))
+    is_publish = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+    
