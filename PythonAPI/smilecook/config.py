e3e382a98e324a80e15a40091a40b571d5ce2a27
colidom <carlos.oliva.dom@gmail.com>
2021-02-02 22:26:54 +0000

    Fix en la config para conectar con la DB

--- PythonAPI/smilecook/config.py
+++ PythonAPI/smilecook/config.py
@@ -1,5 +1,4 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{colidom}:{Pass1234}@localhost/{smilecook}
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{colidom}:{Pass1234}@localhost/{smilecook}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
