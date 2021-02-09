1e18462b082815ce39212d3502459a94d7509b52
colidom <carlos.oliva.dom@gmail.com>
2021-02-09 19:55:29 +0000

    Proyecto renombrado

--- ToDoAPP_Django/ToDo_Project/ToDoAPP_Django/asgi.py
+++ ToDoAPP_Django/ToDo_Project/ToDoAPP_Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ToDo_Project project.
+ASGI config for ToDoAPP_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDo_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDoAPP_Django.settings')
 
 application = get_asgi_application()
--- ToDoAPP_Django/ToDo_Project/ToDoAPP_Django/wsgi.py
+++ ToDoAPP_Django/ToDo_Project/ToDoAPP_Django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ToDo_Project project.
+WSGI config for ToDoAPP_Django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDo_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDoAPP_Django.settings')
 
 application = get_wsgi_application()
