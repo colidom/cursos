bcd91b8e0d3537aaab779d49ed32c53c0b98c98f
colidom <carlos.oliva.dom@gmail.com>
2021-02-12 23:21:12 +0000

    URL Routing

--- CustomerManagementApp/crm1/accounts/views.py
+++ CustomerManagementApp/crm1/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Home")
+
+def products(request):
+    return HttpResponse("Products")
+
+def customers(request):
+    return HttpResponse("Customers")
