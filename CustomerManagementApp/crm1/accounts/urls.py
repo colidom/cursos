7668ec2e83b223cbc8e59aae259cab421769a432
colidom <carlos.oliva.dom@gmail.com>
2021-02-15 22:23:32 +0000

    Urls dinamicas y templates

--- CustomerManagementApp/crm1/accounts/urls.py
+++ CustomerManagementApp/crm1/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<str:pk_test>/', views.customer)
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer, name="customer")
 ]
