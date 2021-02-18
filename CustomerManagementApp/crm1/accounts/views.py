a828dda6024adc0e0b49ccd49f72d65d01b11366
colidom <carlos.oliva.dom@gmail.com>
2021-02-18 00:12:56 +0000

    Formsets en linea

--- CustomerManagementApp/crm1/accounts/views.py
+++ CustomerManagementApp/crm1/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -38,16 +38,20 @@ def customer(request, pk_test):
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
         # print('Printing POST', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
-    context = {'form':form}
+
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
