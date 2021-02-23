d9d59e776534110d81ab0f1b4d4a81fbef4e8ede
colidom <carlos.oliva.dom@gmail.com>
2021-02-23 00:13:20 +0000

    Creado formulario de registro personalizado

--- CustomerManagementApp/crm1/accounts/forms.py
+++ CustomerManagementApp/crm1/accounts/forms.py
@@ -1,8 +1,17 @@
 from django.db.models.base import Model
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
 from .models import *
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+    class Meta():
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
