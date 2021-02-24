61ca2bffc1b5ac6bbc49b4ac5fa9bd98e76e1d69
colidom <carlos.oliva.dom@gmail.com>
2021-02-24 00:26:36 +0000

    Comprobamos que el usuario esté logeado

--- CustomerManagementApp/crm1/accounts/views.py
+++ CustomerManagementApp/crm1/accounts/views.py
@@ -13,30 +13,35 @@ from . filters import OrderFilter
 
 # Create your views here.
 def registerPage(request):
-    form = CreateUserForm()
-
-    if request.method =='POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, f'La cuenta ha sido creada para {user}')
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method =='POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, f'La cuenta ha sido creada para {user}')
+                return redirect('login')
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
 
     context = {}
     return render(request, 'accounts/login.html', context)
