392a0227719d54008feffcf4f4e415f8e00dfa94
colidom <carlos.oliva.dom@gmail.com>
2021-02-24 00:13:15 +0000

    Método logout añadido

--- CustomerManagementApp/crm1/accounts/views.py
+++ CustomerManagementApp/crm1/accounts/views.py
@@ -40,6 +40,11 @@ def loginPage(request):
     context = {}
     return render(request, 'accounts/login.html', context)
 
+def logoutUser(request):
+    logout(request)
+    
+    return redirect('login')
+
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
