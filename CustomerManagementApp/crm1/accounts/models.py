a349a14f12417d961db54b0f01ca56ac630c522d
colidom <carlos.oliva.dom@gmail.com>
2021-03-03 00:59:57 +0000

    Añadido user en el modelo relación OneToOne

--- CustomerManagementApp/crm1/accounts/models.py
+++ CustomerManagementApp/crm1/accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=10, null=True)
     email = models.CharField(max_length=100, null=True)
