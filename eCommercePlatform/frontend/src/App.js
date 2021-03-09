14b6bffe0b4e4b380fa4feb38e69e03abb9e3e6c
colidom <carlos.oliva.dom@gmail.com>
2021-03-09 00:19:13 +0000

    Bootstrap Setup, añadidos componentes cabecera y footer

--- eCommercePlatform/frontend/src/App.js
+++ eCommercePlatform/frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
-    return ( <
-        div >
-        My App <
-        /div>
+    return ( 
+        <div>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome</h1>
+                </Container>
+            </main>
+            <Footer/>
+        </div>
     );
 }
 
---
+++ eCommercePlatform/frontend/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+function Header() {
+    return ( 
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                        <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
