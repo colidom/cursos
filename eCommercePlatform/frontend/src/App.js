a5acf7c34c140497d770bffb5fcfe00ebefb1272
colidom <carlos.oliva.dom@gmail.com>
2021-03-10 23:17:58 +0000

    Listando productos en HomeScreen

--- eCommercePlatform/frontend/src/App.js
+++ eCommercePlatform/frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
     return ( 
@@ -9,7 +10,7 @@ function App() {
             <Header/>
             <main className="py-3">
                 <Container>
-                    <h1>Welcome</h1>
+                    <HomeScreen/>
                 </Container>
             </main>
             <Footer/>
