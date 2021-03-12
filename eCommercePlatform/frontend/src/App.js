af63127b054af5f5f8f4ae33664e41fa45126abb
colidom <carlos.oliva.dom@gmail.com>
2021-03-12 22:56:39 +0000

    React routing implementado

--- eCommercePlatform/frontend/src/App.js
+++ eCommercePlatform/frontend/src/App.js
@@ -1,20 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
     return ( 
-        <div>
+        <Router>
             <Header/>
             <main className="py-3">
                 <Container>
-                    <HomeScreen/>
+                    <Route path='/' component={HomeScreen} exact/>
+                    <Route path='/product/:id' component={ProductScreen}/>
                 </Container>
             </main>
             <Footer/>
-        </div>
+        </Router>
     );
 }
 
--- eCommercePlatform/frontend/src/components/Product.js
+++ eCommercePlatform/frontend/src/components/Product.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Card } from "react-bootstrap";
 import Rating  from "./Rating";
+import { Link } from "react-router-dom";
 
 export default function Product({product}) {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image}/>
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
                 <Card.Text as="div">
                    <div className="my-3">
                         <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
