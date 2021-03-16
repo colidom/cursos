68b8b0a2558b4ba70fca2476bf87ee8d8123959e
colidom <carlos.oliva.dom@gmail.com>
2021-03-16 23:00:05 +0000

    Product updated

--- eCommercePlatform/frontend/src/products.js
+++ eCommercePlatform/frontend/src/products.js
@@ -14,13 +14,13 @@ const products = [
   },
   {
     '_id': '2',
-    'name': 'iPhone 11 Pro 256GB Memory',
+    'name': 'iPhone 12  128GB Memory',
     'image': '/images/phone.jpg',
     'description':
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 12. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     'brand': 'Apple',
     'category': 'Electronics',
-    'price': 599.99,
+    'price': 899.99,
     'countInStock': 7,
     'rating': 4.0,
     'numReviews': 8,
