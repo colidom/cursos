92e0b713a2606752a18504de70549236a3038b30
colidom <carlos.oliva.dom@gmail.com>
2021-03-18 23:07:18 +0000

    Ferching data desde el frontend al backend

--- eCommercePlatform/backend/backend/settings.py
+++ eCommercePlatform/backend/backend/settings.py
@@ -39,12 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
 
     'base.apps.BaseConfig',
        
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
