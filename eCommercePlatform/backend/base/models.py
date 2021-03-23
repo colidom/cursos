db3c13816232279871fa9cdfbc37dc5777e81c04
colidom <carlos.oliva.dom@gmail.com>
2021-03-23 21:06:00 +0000

    Configurado static files del backend

--- eCommercePlatform/backend/base/models.py
+++ eCommercePlatform/backend/base/models.py
@@ -6,7 +6,7 @@ from django.db.models.base import Model
 class Product(models.Model):
    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
    name = models.CharField(max_length=200, null=True, blank=True)
-  # image =
+   image = models.ImageField(null=True, blank=True)
    brand = models.CharField(max_length=200, null=True, blank=True)
    category = models.CharField(max_length=200, null=True, blank=True)
    description = models.TextField(null=True, blank=True)
