c83f01b8f750bc5a5e246325b5ee434518661839
colidom <carlos.oliva.dom@gmail.com>
2021-03-25 22:48:50 +0000

    Componentes Loader y Message

--- eCommercePlatform/frontend/src/screens/HomeScreen.js
+++ eCommercePlatform/frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'  
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { listProducts } from '../actions/productActions'
 
 function HomeScreen() {
@@ -17,8 +19,8 @@ function HomeScreen() {
     return (
         <div>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading...</h2>
-                : error ? <h3>{error}</h3> 
+            {loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message> 
                     :   
                         <Row>
                             {products.map(product => (
