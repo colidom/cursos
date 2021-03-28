233f39ef837a9843423961a29ae9e61674a14ba7
colidom <carlos.oliva.dom@gmail.com>
2021-03-28 23:07:23 +0100

    Añadiendo funcionalidad al carrito

--- eCommercePlatform/frontend/src/screens/CartScreen.js
+++ eCommercePlatform/frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-function CartScreen() {
+function CartScreen({match, location, history}) {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    
+    const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    console.log('cartitems: ', cartItems)
+
+    useEffect(() =>{
+        if(productId){
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
     return (
         <div>
             Cart
