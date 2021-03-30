f015ac0b6742c8450a2ad4d2d2da31e652e822ac
colidom <carlos.oliva.dom@gmail.com>
2021-03-31 00:42:47 +0100

    Cart screen añadir subtotal  y count de products, además de la lógica del checkout

--- eCommercePlatform/frontend/src/screens/CartScreen.js
+++ eCommercePlatform/frontend/src/screens/CartScreen.js
@@ -18,6 +18,15 @@ function CartScreen({match, location, history}) {
             dispatch(addToCart(productId, qty))
         }
     }, [dispatch, productId, qty])
+
+    const removeFromCartHandler = (id) => {
+        console.log('remove: ', id)
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
     return (
         <Row>
             <Col md={8}>
@@ -28,12 +37,69 @@ function CartScreen({match, location, history}) {
                     </Message>
                 ) : (
                     <ListGroup variant='flush'>
-
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+                                    <Col md={3}>
+                                        <Form.Control
+                                            as="select"
+                                            value={item.qty}
+                                            onChange={(e)=> dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                            {
+                                                [...Array(item.countInStock).keys()].map((x) => (
+                                                    <option key={x + 1} value={x + 1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))
+                                            } 
+                                        </Form.Control>
+                                    </Col>
+                                    <Col md={1}>
+                                        <Button
+                                            type='button'
+                                            variant='light'
+                                            onClick={() => removeFromCartHandler(item.product)}
+                                        >
+                                        <i className='fa fa-trash'></i>
+                                            
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        
+                        ))}
                     </ListGroup>
                 )}
             </Col>
             <Col md={4}>
-            
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Subtotal</h2>
+                            <h2>Artículos: ({cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>
+                            {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}€ 
+                        </ListGroup.Item>             
+                    </ListGroup>
+                    <ListGroup.Item>
+                        <Button
+                            type='button'
+                            className='btn-block'
+                            disabled={cartItems.length === 0}
+                            onClick={checkoutHandler}
+                        >
+                            Proceder al pago
+                        </Button>
+                    </ListGroup.Item>
+                </Card>                            
             </Col>
         </Row>
     )
