d7a0c139c3539cf993398e9e993af243e5727202
colidom <carlos.oliva.dom@gmail.com>
2021-04-07 23:07:30 +0100

    Customización JWT

--- eCommercePlatform/backend/base/urls.py
+++ eCommercePlatform/backend/base/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
--- eCommercePlatform/backend/base/views.py
+++ eCommercePlatform/backend/base/views.py
@@ -7,7 +7,23 @@ from .models import Product
 # from .products import products
 from .serializers import ProductSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
