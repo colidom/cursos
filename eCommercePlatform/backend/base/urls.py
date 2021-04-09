71c3222a1031be277a4465717d22dcf1da54c01e
colidom <carlos.oliva.dom@gmail.com>
2021-04-09 23:29:11 +0100

    Protegiendo rutas para getUserProfile y getUsers

--- eCommercePlatform/backend/base/urls.py
+++ eCommercePlatform/backend/base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
     path('users/profile/', views.getUserProfile, name='user-profile'),
+    path('users/', views.getUsers, name='users'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
     
--- eCommercePlatform/backend/base/views.py
+++ eCommercePlatform/backend/base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 # from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -42,9 +44,18 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
