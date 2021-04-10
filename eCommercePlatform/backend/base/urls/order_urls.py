101047bc21e06011e8c51373fe1d6eb2053fbd2e
colidom <carlos.oliva.dom@gmail.com>
2021-04-10 02:11:03 +0100

    Reorganización del proyecto, urls y views

---
+++ eCommercePlatform/backend/base/urls/order_urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from ..views import orders_views as views
+
+urlpatterns = [
+    
+]
--- eCommercePlatform/backend/base/views/user_views.py
+++ eCommercePlatform/backend/base/views/user_views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
-from .models import Product
-# from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from ..serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -15,7 +12,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 
-# Create your views here.
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -30,20 +27,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-    
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id/',
-        '/api/products/<update>/<id>',
-    ]
-    return Response(routes)
+
 
 @api_view(['POST'])
 def registerUser(request):
@@ -79,19 +63,3 @@ def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-
-    return Response(serializer.data)
-    
