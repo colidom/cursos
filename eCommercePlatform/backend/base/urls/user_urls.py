52dfd17097c29fd1a8baa8199d2c41bd677ff522
colidom <carlos.oliva.dom@gmail.com>
2021-04-15 23:05:25 +0100

    Arreglado bug en la ruta del backend que generaba error "Request failed with status code 404" al registrar

--- eCommercePlatform/backend/base/urls/user_urls.py
+++ eCommercePlatform/backend/base/urls/user_urls.py
@@ -4,7 +4,7 @@ from ..views import user_views as views
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     
-    path('register', views.registerUser, name='register'),
+    path('register/', views.registerUser, name='register'),
 
     path('profile/', views.getUserProfile, name='user-profile'),
     path('', views.getUsers, name='users'),
