e8b0c0542e20c41427c9b4d2cf1363de042d8bb4
colidom <carlos.oliva.dom@gmail.com>
2021-04-19 23:56:08 +0100

    userDetails constantes, reducer y registro de reducer en Store

--- eCommercePlatform/frontend/src/reducers/userReducers.js
+++ eCommercePlatform/frontend/src/reducers/userReducers.js
@@ -9,6 +9,10 @@ import {
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
 
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+
 } from '../constants/userConstants'
 
 
@@ -44,6 +48,22 @@ export const userRegisterReducer = (state = {}, action) =>{
         
         case USER_LOGOUT:
             return {}
+
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state = {user: {} }, action) =>{
+    switch(action.type){
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
 
         default:
             return state
