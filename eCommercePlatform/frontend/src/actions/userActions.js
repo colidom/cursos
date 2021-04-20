38bff78ea0a4ffbbc90fc9cfff0e1ef0a7dcdc50
colidom <carlos.oliva.dom@gmail.com>
2021-04-20 23:01:31 +0100

    Update user Profile

--- eCommercePlatform/frontend/src/actions/userActions.js
+++ eCommercePlatform/frontend/src/actions/userActions.js
@@ -14,6 +14,11 @@ import {
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
     
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
+
 } from '../constants/userConstants'
 
 
@@ -131,6 +136,51 @@ export const getUserDetails = (id) => async(dispatch, getState) => {
     } catch (error) {
         dispatch({
             type:USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
+
+export const updateUserProfile = (user) => async(dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const {
+            userLogin: { userInfo }
+        } = getState()
+
+        const config = {
+            headers:{
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.PUT(
+            `/api/users/profile/update/`,
+            user,
+            config
+            )
+
+        dispatch({
+            type:USER_UPDATE_PROFILE_SUCCESS,
+            payload:data
+        })
+
+        dispatch({
+            type:USER_LOGIN_SUCCESS,
+            payload:data
+        })
+        
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type:USER_UPDATE_PROFILE_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
--- eCommercePlatform/frontend/src/screens/ProfileScreen.js
+++ eCommercePlatform/frontend/src/screens/ProfileScreen.js
@@ -4,7 +4,8 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 function ProfileScreen(history) {
     const [name, setName] = useState('')
@@ -21,18 +22,22 @@ function ProfileScreen(history) {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
+
     useEffect(() => {
         if (!userInfo){
             history.push('/login')
         }else{
-            if (!user || !user.name){
+            if (!user || !user.name || success){
+                dispatch({ type:USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
             }else{
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -40,7 +45,12 @@ function ProfileScreen(history) {
         if(password !== confirmPassword){
             setMessage('¡Las contraseñas introducidas no coinciden!')
         }else{
-            console.log('Updating...')
+            dispatch(updateUserProfile({
+                'id':user._id,
+                'name':name,
+                'email':email,
+                'password':password
+            }))
         }
     }
 
