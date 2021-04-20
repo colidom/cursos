6316f8d0682f0771829b9bcf79463ee39d47acdc
colidom <carlos.oliva.dom@gmail.com>
2021-04-20 23:28:45 +0100

    User Details bug fixed

--- eCommercePlatform/frontend/src/actions/userActions.js
+++ eCommercePlatform/frontend/src/actions/userActions.js
@@ -13,6 +13,7 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
     
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
@@ -60,7 +61,8 @@ export const login = (email, password) => async(dispatch) => {
 
 export const logOut = () => (dispatch) => {
     localStorage.removeItem('userInfo')
-    dispatch({type:USER_LOGOUT})
+    dispatch({ type:USER_LOGOUT })
+    dispatch({ type:USER_DETAILS_RESET })
 }
 
 export const register = (name, email, password) => async(dispatch) => {
