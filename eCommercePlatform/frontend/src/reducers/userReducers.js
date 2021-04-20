df0a8995374b5d4f941f7d93288e7aa6cd877e20
colidom <carlos.oliva.dom@gmail.com>
2021-04-20 22:47:44 +0100

    userUpdate profile, constantes, redux añadidos

--- eCommercePlatform/frontend/src/reducers/userReducers.js
+++ eCommercePlatform/frontend/src/reducers/userReducers.js
@@ -13,6 +13,11 @@ import {
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
 
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
+
 } from '../constants/userConstants'
 
 
@@ -69,3 +74,23 @@ export const userDetailsReducer = (state = {user: {} }, action) =>{
             return state
     }
 }
+
+export const userUpdateProfileReducer = (state = { }, action) =>{
+    switch(action.type){
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
+        
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
+
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
+            
+        default:
+            return state
+    }
+}
+
--- eCommercePlatform/frontend/src/store.js
+++ eCommercePlatform/frontend/src/store.js
@@ -3,7 +3,12 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
+import { 
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer 
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,6 +17,7 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
