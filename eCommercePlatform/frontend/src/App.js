149b5a713fb853d9545ad4a983a8cccc44922437
colidom <carlos.oliva.dom@gmail.com>
2021-04-22 22:34:24 +0100

    Añadido routing para shipping screen

--- eCommercePlatform/frontend/src/App.js
+++ eCommercePlatform/frontend/src/App.js
@@ -10,6 +10,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 function App() {
     return ( 
@@ -21,6 +22,7 @@ function App() {
                     <Route path='/login' component={LoginScreen}/>
                     <Route path='/register' component={RegisterScreen}/>
                     <Route path='/profile' component={ProfileScreen}/>
+                    <Route path='/shipping' component={ShippingScreen}/>
                     <Route path='/product/:id' component={ProductScreen}/>
                     <Route path='/cart/:id?' component={CartScreen}/>
                 </Container>
---
+++ eCommercePlatform/frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShippingScreen() {
+    return (
+        <div>
+            Shipping
+        </div>
+    )
+}
+
+export default ShippingScreen
