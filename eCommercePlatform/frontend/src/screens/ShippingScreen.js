d41bb1f5f1cb25cdfa6e67197e062e11196a2b2e
colidom <carlos.oliva.dom@gmail.com>
2021-04-22 23:18:55 +0100

    Component Checkut Steps

--- eCommercePlatform/frontend/src/screens/ShippingScreen.js
+++ eCommercePlatform/frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from '../actions/cartActions'
 
 function ShippingScreen({ history }) {
@@ -29,6 +30,7 @@ function ShippingScreen({ history }) {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Envío</h1>
             <Form onSubmit={submitHandler}>
 
