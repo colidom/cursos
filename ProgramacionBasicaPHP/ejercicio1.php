cc59bd7e0f23883412af8ea52a44e51a00432e7f
colidom <carlos.oliva.dom@gmail.com>
2021-04-28 23:37:34 +0100

    Ejercicio 1

---
+++ ProgramacionBasicaPHP/ejercicio1.php
@@ -0,0 +1,17 @@
+<h2>Ejercicio 1</h2>
+<?php
+/*Crea dos variables cuyo nombre sea "uno" y "dos" e imprímelas
+por pantalla. Pon un comentario comentario con el tipo de dato
+que contienen.
+*/
+
+// String
+$uno = 'Contenido de la variable 1';
+
+// Integer
+$dos = 120;
+
+echo 'UNO: '.$uno."</br>";
+echo 'DOS: '.$dos."</hr>";
+
+?>
