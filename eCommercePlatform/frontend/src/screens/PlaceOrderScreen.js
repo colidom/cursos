c618179dd79f01830f6eaf5f0a56bcc6f1adb943
colidom <carlos.oliva.dom@gmail.com>
2021-04-29 23:39:45 +0100

    Funcionalidad pagina hacer pedido añadida

--- eCommercePlatform/frontend/src/screens/PlaceOrderScreen.js
+++ eCommercePlatform/frontend/src/screens/PlaceOrderScreen.js
@@ -7,9 +7,119 @@ import CheckoutSteps from "../components/CheckoutSteps";
 
 function PlaceOrderScreen() {
     const cart = useSelector(state => state.cart)
+
+    cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
+    cart.shippingPrice = (cart.itemsPrice > 100 ? 0 : 10).toFixed(2)
+    cart.taxPrice = Number((0.21) * cart.itemsPrice).toFixed(2)
+
+    cart.totalPrice = Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)
+
+    const placeOrder = () => {
+        console.log('placed order')
+    }
+
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4/>
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Dirección de envío</h2>
+                            <p>
+                                <strong>Enviar a: </strong>
+                                {cart.shippingAddress.address}, {cart.shippingAddress.city},
+                                {'   '}
+                                {cart.shippingAddress.postalCode},
+                                {'   '}
+                                {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Método de pago:</h2>
+                            <p>
+                                <strong>Pagar con: </strong>
+                                {cart.paymentMethod}
+                            </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Artículos:</h2>
+                            {cart.cartItems.length === 0 ? <Message variant='info'>
+                                Su carrito está vacío
+                            </Message> : (
+                               <ListGroup variant='flush'>
+                                {cart.cartItems.map((item, index) => (
+                                    <ListGroup.Item key={index}>
+                                        <Row>
+                                            <Col md={1}>
+                                                <Image src={item.image} alt={item.name} fluid rounded/>
+                                            </Col>
+                                            <Col>
+                                                <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            </Col>
+                                            <Col md={4}>
+                                                {item.qty} X {item.price}€ = {(item.qty * item.price).toFixed(2)}€
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                ))}
+                               </ListGroup>
+                            )}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            
+                <Col md={4}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>Resumen</h2>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Items:</Col>
+                                    <Col>{cart.itemsPrice}€</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Envío:</Col>
+                                    <Col>{cart.shippingPrice}€</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Impuestos:</Col>
+                                    <Col>{cart.taxPrice}€</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total:</Col>
+                                    <Col>{cart.totalPrice}€</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button
+                                    type='button'
+                                    className='btn-block'
+                                    disabled={cart.cartItems === 0}
+                                    onClick={placeOrder}
+                                >
+                                    Hacer el pedido
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
