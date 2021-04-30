84afb9f7e3c3bb72fd2f57e1d1927732c7c48d0f
colidom <carlos.oliva.dom@gmail.com>
2021-04-30 23:28:49 +0100

    Añadida vista orderViews

---
+++ eCommercePlatform/backend/base/views/orders_views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from ..models import Product, Order, OrderItem, ShippingAddress
+from ..serializers import ProductSerializer
+
+from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail':'No hay artículos'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # (1) Create order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice= data['shippingPrice'],
+            totalPrice=data['totalPrice']
+        )
+        # (2) Create shipping address
+        # (3) Create order items and set the orderItem relationship
+        # (4) Update stock
+
+    return Response('ORDER')
