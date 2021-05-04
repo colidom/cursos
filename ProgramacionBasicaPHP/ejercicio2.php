3cd711a09843b447bde0592fe250a0386a6e95b6
Carlos Oliva <carlos.oliva@correos.com>
2021-05-04 14:54:35 +0100

    Ejercicio2 Bucle for

---
+++ ProgramacionBasicaPHP/ejercicio2.php
@@ -0,0 +1,12 @@
+<h2>Ejercicio 2</h2>
+<?php
+/*Escribe un programa que imprima por pantalla 
+los cradrados(el número multiplicado por sí mismo)
+de los 30 primeros números naturales
+*/
+
+for($i = 1; $i <= 30; $i++) {
+    echo "El cuadrado de ". $i.' es '.($i*$i)."<br/>";
+
+}
+?>
