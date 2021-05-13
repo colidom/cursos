e08520be8433c348db05b56cf4340b687e385f4f
colidom <carlos.oliva.dom@gmail.com>
2021-05-13 23:37:12 +0100

    Añadidos serializadores de ShippingAddressSerializer, OrderItemSerializer y OrderSerializer

--- eCommercePlatform/backend/base/serializers.py
+++ eCommercePlatform/backend/base/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Product
+from .models import Product, Order, OrderItem, ShippingAddress
 
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
@@ -39,6 +39,24 @@ class UserSerializerWithToken(UserSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
