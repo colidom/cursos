c46b3482804a5609e5b519d5d584345fc76cc0ea
colidom <carlos.oliva.dom@gmail.com>
2021-05-17 23:23:36 +0100

    Registrado orderReducer en el store

--- eCommercePlatform/frontend/src/store.js
+++ eCommercePlatform/frontend/src/store.js
@@ -9,6 +9,7 @@ import {
     userDetailsReducer, 
     userUpdateProfileReducer 
 } from './reducers/userReducers'
+import { orderCreateReducers } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -18,6 +19,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducers,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
