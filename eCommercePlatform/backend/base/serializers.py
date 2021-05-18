9405c76c2b04874a1c9765d5eccd329724902316
colidom <carlos.oliva.dom@gmail.com>
2021-05-18 23:28:43 +0100

    fix variable y metodo

--- eCommercePlatform/backend/base/serializers.py
+++ eCommercePlatform/backend/base/serializers.py
@@ -57,7 +57,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    orders = serializers.SerializerMethodField(read_only=True)
+    orderItems = serializers.SerializerMethodField(read_only=True)
     shippingAddress = serializers.SerializerMethodField(read_only=True)
     user = serializers.SerializerMethodField(read_only=True)
 
@@ -65,7 +65,7 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-    def get_orders(self, obj):
+    def get_orderItems(self, obj):
         items = obj.orderitem_set.all()
         serializer = OrderItemSerializer(items, many=True)
         return serializer.data
