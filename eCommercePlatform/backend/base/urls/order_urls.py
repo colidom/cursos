68636c1462726312561d463264b451d05041a8f0
colidom <carlos.oliva.dom@gmail.com>
2021-05-25 22:30:57 +0100

    Añadida url para el método getOrderById

--- eCommercePlatform/backend/base/urls/order_urls.py
+++ eCommercePlatform/backend/base/urls/order_urls.py
@@ -3,5 +3,5 @@ from ..views import orders_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),
-    
+    path('<str:pk>/', views.getOrderById, name='user-order'),
 ]
