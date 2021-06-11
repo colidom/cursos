a41a3a21ec3706d9d8932bd6c27d58ca83d3df95
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2021-06-11 23:53:25 +0100

    First commit

---
+++ iOS-Calculator/iOS-Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Calculator
+//
+//  Created by Carlos Javier Oliva Domínguez on 11/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Calculator/iOS-Calculator/iOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorApp.swift
+//  iOS-Calculator
+//
+//  Created by Carlos Javier Oliva Domínguez on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
