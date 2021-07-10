b81052ed928e738f40e9852445c5e7b27aac44cf
colidom <carlos.oliva.dom@gmail.com>
2021-07-10 19:42:31 +0100

    Fix en getOrderById method response

--- eCommercePlatform/backend/base/views/orders_views.py
+++ eCommercePlatform/backend/base/views/orders_views.py
@@ -71,4 +71,4 @@ def getOrderById(request, pk):
             Response({'detail':'No autorizado para ver esta orden'}, 
             status=status.HTTP_400_BAD_REQUEST)
     except:
-        return Response({'detail': 'La orden no existe'}, status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'La orden no existe'}, status=status.HTTP_400_BAD_REQUEST)
