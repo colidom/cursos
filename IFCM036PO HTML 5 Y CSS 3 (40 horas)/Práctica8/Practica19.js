fcad120720539828c831022738f3a9c377d459a0
Carlos Oliva <carlos.oliva.dom@gmail.com>
2022-01-21 23:21:16 +0000

    Añadidos recursos práctica 8

---
+++ IFCM036PO HTML 5 Y CSS 3 (40 horas)/Práctica8/Practica19.js
@@ -0,0 +1,13 @@
+﻿/* Seleccionar elementos */
+var elemento = document.getElementById("content");
+var option1 = document.getElementById("option-1");
+var option2 = document.getElementById("option-2");
+var option3 = document.getElementById("option-3");
+
+/* Añadir manejadores de evento utilizando el método toggle de la propiedad classList */
+/* El método toggle de la propiedad classList añade o quita la clase
+   indicada en función de si el elemento la tiene aplicada o no.
+*/
+option1.addEventListener("change", function () {
+        elemento.classList.toggle("border");
+}, false);
