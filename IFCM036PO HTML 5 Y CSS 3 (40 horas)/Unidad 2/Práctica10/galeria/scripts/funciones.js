dbd91182a5e4bdab987334177cc41597593dca26
Carlos Oliva <carlos.oliva.dom@gmail.com>
2022-02-02 17:19:02 +0000

    Añadidos recursos práctica 10

---
+++ IFCM036PO HTML 5 Y CSS 3 (40 horas)/Unidad 2/Práctica10/galeria/scripts/funciones.js
@@ -0,0 +1,31 @@
+﻿function PrepararHipervinculos() {
+  if (!document.getElementById || !document.getElementsByTagName) return false;
+  var hipervinculos = document.getElementsByTagName("a");
+  for (var i = 0; i < hipervinculos.length; i++) {
+    hipervinculos[i].addEventListener("click", function (e) {
+                                                  e.preventDefault();
+                                                  MostrarFoto(this);
+                                              },
+                                      false)
+    }
+}
+function MostrarFoto(elemento) {
+  var source = elemento.firstChild.getAttribute("src");
+  source = source.replace("thumbnails/", "");
+  document.getElementById("actual").setAttribute("src", source);
+  Describir(elemento.firstChild.getAttribute("id"));
+}
+function Describir(id) {
+  var texto = document.getElementById(id).getAttribute("alt");
+  var objeto = document.getElementById("descripcion");
+  if (!objeto) {
+      var newText = document.createTextNode(texto);
+      var newElement = document.createElement("p");
+      newElement.setAttribute("id", "descripcion");
+      newElement.setAttribute("class", "textoDescripcion");
+      newElement.appendChild(newText);
+      document.body.appendChild(newElement);
+  } else {
+      objeto.firstChild.nodeValue = texto;
+  }  
+}
