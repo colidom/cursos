2d57b0febfa9f7c398878f7f8a495977d08e4feb
Carlos Oliva <carlos.oliva.dom@gmail.com>
2022-03-08 23:54:30 +0000

    Variables en JavaScript

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/variables.js
@@ -0,0 +1,22 @@
+// Variables
+
+var product = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+var disponible; // Iniciamos la variable sin asignarle valor
+product = true; // Reasignando el valor de la variable
+
+var product1 = 'Ordenador',
+    in_stock = 10,
+    category = 'Ordenadores';
+
+// Formas de crear variables:
+// Error:
+//var 1disponible; // Error, no se pueden iniciar con un número
+//var -disponible; // Error, no se pueden iniciar con un -
+
+// Estilos para las variables
+var produc_name = 'Monitor HD'; // Underscore
+var productName = 'Monitor HD'; // Camel Case
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var NombreProducto = 'Monitor HD';
+
+console.log(produc_name);
