c219905206abb0248537630b3603b11989d66871
colidom <carlos.oliva.dom@outlook.es>
2022-03-14 20:14:33 +0000

    let variables

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/let_variables.js
@@ -0,0 +1,22 @@
+// Variables
+
+let product = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+let disponible; // Iniciamos la variable sin asignarle valor
+product = true; // Reasignando el valor de la variable
+
+let product1 = 'Ordenador',
+    in_stock = 10,
+    category = 'Ordenadores';
+
+// Formas de crear variables:
+// Error:
+//var 1disponible; // Error, no se pueden iniciar con un número
+//var -disponible; // Error, no se pueden iniciar con un -
+
+// Estilos para las variables
+let produc_name = 'Monitor HD'; // Underscore
+let productName = 'Monitor HD'; // Camel Case
+let NombreProducto = 'Monitor HD'; // Pascal Case
+let nombreProducto = 'Monitor HD';
+
+console.log(produc_name);
