f42cdadf8b3441565f8c8eb53819b292bc1440ae
colidom <carlos.oliva.dom@outlook.es>
2022-03-14 20:21:20 +0000

    const variables

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/const_variables.js
@@ -0,0 +1,22 @@
+// Variables
+
+const product = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+const disponible = true; // Iniciamos la variable sin asignarle valor
+product = true; // Reasignando el valor de la variable
+
+const product1 = 'Ordenador',
+    in_stock = 10,
+    category = 'Ordenadores';
+
+// Formas de crear variables:
+// Error:
+//var 1disponible; // Error, no se pueden iniciar con un número
+//var -disponible; // Error, no se pueden iniciar con un -
+
+// Estilos para las variables
+const produc_name = 'Monitor HD'; // Underscore
+const productName = 'Monitor HD'; // Camel Case
+const NombreProducto = 'Monitor HD'; // Pascal Case
+const nombreProducto = 'Monitor HD';
+
+console.log(produc_name);
