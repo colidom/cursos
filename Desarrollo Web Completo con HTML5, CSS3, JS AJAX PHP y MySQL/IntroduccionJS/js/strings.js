11e775f4e54e749135362011fcd549fa8b20c0e1
colidom <carlos.oliva.dom@outlook.es>
2022-03-15 23:25:23 +0000

    Strings o Cadenas de Texto

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/strings.js
@@ -0,0 +1,8 @@
+// String o Cadenas de Texto
+const product = 'Monitor de 20 pulgadas';
+const product2 = String('Monitor 30 pulgadas');
+const product3 = new String('Monitor 50 pulgadas');
+
+console.log(product); // String
+console.log(product2); // String
+console.log(product3);  // Object
