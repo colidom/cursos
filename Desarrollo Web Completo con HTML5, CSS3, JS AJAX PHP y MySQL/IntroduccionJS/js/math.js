d27e910828e8c8d82768b8c95001e66d33095ecd
colidom <carlos.oliva.dom@outlook.es>
2022-03-16 23:58:41 +0000

    El Objeto Math en JS

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/math.js
@@ -0,0 +1,14 @@
+// Objeto math
+let result;
+
+result = Math.PI;
+result = Math.round(2.5); // Redondea
+result = Math.ceil(2.5); // Redondea siempre hacia arriba
+result = Math.floor(2.5); // Redondea siempre hacia abajo
+result = Math.sqrt(2.5); // Raiz cuadrada
+result = Math.abs(-2.5); // Convierne un númpero negativo en positivo
+result = Math.min(2.5, 10, 1000, 1222); // Devuelve el vamor menor
+result = Math.random(); // Devuelve un número aleatorio
+result = Math.floor(Math.random() * 30); // Devuelve un número aleatorio multiplicado por 30
+
+console.log(result);
