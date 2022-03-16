7cf899d1822db375d459f33a023f5882a6099a1b
colidom <carlos.oliva.dom@outlook.es>
2022-03-16 23:49:50 +0000

    Números y Operadores

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/numbers.js
@@ -0,0 +1,16 @@
+const user = "Colidom";
+const num1 = 100;
+const num2 = 200;
+const num3 = 200.20;
+
+console.log(user);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+// Operaciones
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num2 * num1);
+console.log(num2 / num1);
+console.log(num2 % num1);
