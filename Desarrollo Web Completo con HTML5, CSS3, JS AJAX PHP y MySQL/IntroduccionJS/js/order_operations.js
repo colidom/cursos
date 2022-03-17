157d67cdec0aaa438d35081d5d5a7b755bae3efc
colidom <carlos.oliva.dom@outlook.es>
2022-03-17 23:25:28 +0000

    Controlar el Orden de las operaciones

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/order_operations.js
@@ -0,0 +1,18 @@
+// Orden de las operaciones
+let result;
+
+result = (20 + 30) * 2
+
+igic = (100 + 300 + 50 + 70)  * 7 / 100
+
+console.log(result);
+console.log(igic);
+
+
+// Incrementos
+let points = 10;
+points ++; // 11
+++ points; // 12
+points += 1; // 13
+
+console.log(points);
