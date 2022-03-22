40d706cefc6a186ca65c3591aa7ffe4a31be6f29
colidom <colidom@outlook.com>
2022-03-22 21:42:52 +0000

    Objetos js

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/objects.js
@@ -0,0 +1,25 @@
+// Objetos
+const productName = "Monitor 20 pulgadas";
+const price = 300;
+const stock = true;
+
+const product = {
+    productName : "Monitor 20 pulgadas",
+    price : 300,
+    stock : true
+}
+// Acceso a datos
+// Forma 1
+console.log(product);
+console.log(product.productName);
+console.log(product.price);
+console.log(product.stock);
+
+// Forma 2
+console.log(product['price']);
+
+// Bucle
+for (p in product){
+    console.log(p);
+}
+
