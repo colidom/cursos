271ee93d3fb0d3e8109bf54e0580e9bec9f3854d
colidom <colidom@outlook.com>
2022-03-25 23:50:55 +0000

    Arrays

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/arrays.js
@@ -0,0 +1,46 @@
+// Arreglos o Arrays
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers);
+console.table(numbers);
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+console.table(months);
+
+const arreglo = [
+  "Hola",
+  10,
+  true,
+  "si",
+  null,
+  { Nombre: "Juan", Trabajo: "Programador" },
+  [1, 2, 3],
+];
+console.log(arreglo);
+
+// Acceder a los valores d eun arreglo
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+
+// Conocer la extensión de un arreglo
+console.log(months.length);
+
+months.forEach(function (months) {
+  console.log(months);
+});
