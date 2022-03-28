5873f577d8ccfa6aa3d78fe3764c47ab1fe54e38
colidom <colidom@outlook.com>
2022-03-28 23:10:51 +0100

    Más Métodos de Arrays

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/objects_methods_2.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/objects_methods_2.js
@@ -32,22 +32,28 @@ months.forEach(function (month) {
 });
 
 // Includes
-let result = months.includes('March');
+let result = months.includes("March");
 
 // Some ideal para arreglo de objetos
-const result2 = cart.some(function(product) {
-    return product.name === 'Tablet';
+const result2 = cart.some(function (product) {
+  return product.name === "Tablet";
 });
 console.log(result2);
 
 /* Lo mismo pero con arrow function */
-const result3 = cart.some(product => product.name === 'Tablet');
+const result3 = cart.some((product) => product.name === "Tablet");
 console.log(result3);
 
 // Reducer
-result = cart.reduce(function(total, product) {
-    return total + product.price
+result = cart.reduce(function (total, product) {
+  return total + product.price;
 }, 0);
 
 console.log(result);
 
+// Filter
+result = cart.filter(function (product) {
+  return product.price > 400;
+});
+
+console.log(result);
