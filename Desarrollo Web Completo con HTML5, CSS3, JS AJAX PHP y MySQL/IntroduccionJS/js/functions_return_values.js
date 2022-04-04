e171caa16a6d6d109c530d1f9b67742fa35bc8ac
colidom <colidom@outlook.com>
2022-04-04 21:55:53 +0100

    Funciones que retornan valores

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/functions_return_values.js
@@ -0,0 +1,26 @@
+function sum(n1, n2) {
+    return n1 + n2
+}
+
+const result = sum(2, 3);
+
+console.log(result);
+
+let total = 0;
+
+function addCart(price) {
+    return total += price;
+}
+
+function calculateVat(total) {
+    return 1.15 * total;
+}
+
+total = addCart(200);
+total = addCart(400);
+total = addCart(600);
+
+console.log(total);
+const totalPayment = calculateVat(total);
+
+console.log(totalPayment);
