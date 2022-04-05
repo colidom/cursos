ebbee031c2575d4f2bac1d18920830c22a809387
colidom <colidom@outlook.com>
2022-04-05 21:10:25 +0100

    Arrow Functions

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/arrow_functions.js
@@ -0,0 +1,15 @@
+// Arrow Functions - Funciones flecha
+const sum = (n1, n2) => { // Forma 1
+    console.log(n1 + n2);
+}
+
+const sub = (n1, n2) => console.log(n1 - n2); // Forma 2
+
+sum(5, 10);
+sub(5, 10);
+
+const learning = (lenguaje) => {
+    console.log("Aprendiendo " + lenguaje)
+}
+
+learning("JavaScript")
