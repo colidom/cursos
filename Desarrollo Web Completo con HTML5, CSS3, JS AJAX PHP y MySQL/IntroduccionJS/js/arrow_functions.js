03fa56cdf13674a18e558887aac396008669d6b5
colidom <colidom@outlook.com>
2022-04-05 21:13:18 +0100

    Arrow Functions

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/arrow_functions.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/arrow_functions.js
@@ -13,3 +13,34 @@ const learning = (lenguaje) => {
 }
 
 learning("JavaScript")
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+const cart = [
+{ name: "Monitor 20 pulgadas", price: 500 },
+{ name: "Televisión 50 pulgadas", price: 700 },
+{ name: "Tablet", price: 300 },
+{ name: "Auriculares", price: 200 },
+{ name: "Teclado", price: 50 },
+{ name: "Teléfono Móvil", price: 500 },
+{ name: "Portatil", price: 800 },
+];
+
+months.forEach(month => {
+    if (month == 'March') {
+        console.log("Marzo si existe");
+    }
+});
