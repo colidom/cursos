21d7cff91e125ddc6a501ffbe982634d7c691fc6
colidom <colidom@outlook.com>
2022-04-06 23:27:16 +0100

    Estructuras de Control

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/control_structures.js
@@ -0,0 +1,8 @@
+// Estructuras de control
+const points = 1000;
+
+if (points == 1000) { // == que sea el mismo valos y === comprueba además el tipo de dato
+    console.log("Si los puntos son 1000");
+} else {
+    console.log("No es igual");
+}
