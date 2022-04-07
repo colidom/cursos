d7bd0e7e47eb200b2f240d57e8ae7059c2ba5006
colidom <colidom@outlook.com>
2022-04-07 22:08:19 +0100

    For Loops

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/forLoop.js
@@ -0,0 +1,12 @@
+// For loop
+/* for(let i = 0; i < 10; i++) {
+    console.log(i);
+} */
+
+for(let i = 0; i < 10; i++) {
+    if(i % 2 === 0) {
+        console.log("Par");
+    } else {
+        console.log("Impar");
+    }
+}
