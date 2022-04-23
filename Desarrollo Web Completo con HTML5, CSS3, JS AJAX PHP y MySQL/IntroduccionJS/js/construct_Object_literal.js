a038842825b0a5cd18996a14ffdffaed1e33e649
colidom <colidom@outlook.com>
2022-04-23 23:21:43 +0100

    Formateo y añadido prettierrc

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
@@ -2,37 +2,37 @@
 
 // Object Literal
 const PRODUCT = {
-  name: "Tablet",
-  price: 500,
-};
+    name: 'Tablet',
+    price: 500,
+}
 
 // Object constructor
 function Customer(name, surname) {
-  this.name = name;
-  this.surname = surname;
+    this.name = name
+    this.surname = surname
 }
 
 Customer.prototype.formatCustomer = function () {
-  return `El cliente ${this.name} ${this.surname}`;
-};
+    return `El cliente ${this.name} ${this.surname}`
+}
 
 function Product(name, price) {
-  this.name = name;
-  this.price = price;
+    this.name = name
+    this.price = price
 }
 
 // Crear funciones que solo se utilizan en un objeto en específico
 Product.prototype.formatProduct = function () {
-  return `El producto ${this.name} tiene un precio de ${this.price}€`;
-};
+    return `El producto ${this.name} tiene un precio de ${this.price}€`
+}
 
-const product2 = new Product("Monitor Curvo de 49", 800);
-const product3 = new Product("Monitor QHD 35", 300);
-const customer = new Customer("Carlos", "Oliva");
+const product2 = new Product('Monitor Curvo de 49', 800)
+const product3 = new Product('Monitor QHD 35', 300)
+const customer = new Customer('Carlos', 'Oliva')
 
-console.log(product2);
-console.log(customer);
-console.log(product2.formatProduct());
-console.log(product3.formatProduct());
-console.log(customer.formatCustomer());
+console.log(product2)
+console.log(customer)
+console.log(product2.formatProduct())
+console.log(product3.formatProduct())
+console.log(customer.formatCustomer())
 /* console.log(formatProduct(product2)); */
