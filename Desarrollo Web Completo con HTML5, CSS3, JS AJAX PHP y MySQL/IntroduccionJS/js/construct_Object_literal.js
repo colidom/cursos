287dc2a5f6440b09b6c3c0a9c9bb8a6ef9702c24
colidom <colidom@outlook.com>
2022-04-23 23:21:23 +0100

    Formateo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
@@ -2,30 +2,29 @@
 
 // Object Literal
 const PRODUCT = {
-    name: 'Tablet',
-    price: 500
-}
+  name: "Tablet",
+  price: 500,
+};
 
 // Object constructor
 function Customer(name, surname) {
-    this.name = name;
-    this.surname = surname;
+  this.name = name;
+  this.surname = surname;
 }
 
-Customer.prototype.formatCustomer = function() {
-    return `El cliente ${this.name} ${this.surname}`;
-}
-
+Customer.prototype.formatCustomer = function () {
+  return `El cliente ${this.name} ${this.surname}`;
+};
 
 function Product(name, price) {
-    this.name = name;
-    this.price = price;
+  this.name = name;
+  this.price = price;
 }
 
 // Crear funciones que solo se utilizan en un objeto en específico
-Product.prototype.formatProduct = function() {
-    return `El producto ${this.name} tiene un precio de ${this.price}€`;
-}
+Product.prototype.formatProduct = function () {
+  return `El producto ${this.name} tiene un precio de ${this.price}€`;
+};
 
 const product2 = new Product("Monitor Curvo de 49", 800);
 const product3 = new Product("Monitor QHD 35", 300);
