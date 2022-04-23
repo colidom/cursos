829cc8c99071a107be8fcb760ee9c0692a73dcbe
colidom <colidom@outlook.com>
2022-04-23 23:16:33 +0100

    Prototypes

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/construct_Object_literal.js
@@ -7,13 +7,38 @@ const PRODUCT = {
 }
 
 // Object constructor
+function Customer(name, surname) {
+    this.name = name;
+    this.surname = surname;
+}
+
+Customer.prototype.formatCustomer = function() {
+    return `El cliente ${this.name} ${this.surname}`;
+}
+
+
 function Product(name, price) {
     this.name = name;
     this.price = price;
 }
 
+
+// Crear funciones que solo se utilizan en un objeto en específico
+Product.prototype.formatProduct = function() {
+    return `El producto ${this.name} tiene un precio de ${this.price}€`;
+}
+
+
 const product2 = new Product("Monitor Curvo de 49", 800);
 const product3 = new Product("Monitor QHD 35", 300);
 
-console.log(product2);
-console.log(product3);
+const customer = new Customer("Carlos", "Oliva");
+console.log(customer);
+
+function formatProduct(product) {
+    return `El producto ${product.name} tiene un precio de ${product.price}€`;
+}
+
+console.log(product2.formatProduct());
+console.log(product3.formatProduct());
+/* console.log(formatProduct(product2)); */
