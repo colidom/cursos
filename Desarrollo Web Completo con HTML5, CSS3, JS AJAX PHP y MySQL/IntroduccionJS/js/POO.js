ade78c3dd2c04ff3ba962304dbbc833716b516a6
colidom <colidom@outlook.com>
2022-04-27 23:44:54 +0100

    POO - Classes en JavaScript

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/POO.js
@@ -0,0 +1,13 @@
+// Clases
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const product1 = new Product('Monitor Curvo de 49', 800)
+const product2 = new Product('Monitor QHD 35', 300)
+
+console.log(product1);
+console.log(product2);
