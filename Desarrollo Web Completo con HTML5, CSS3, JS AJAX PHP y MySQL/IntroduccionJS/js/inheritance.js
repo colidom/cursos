b9bf4fc348c636336668d74daeeb5ca801fa090a
colidom <colidom@outlook.com>
2022-04-28 00:02:25 +0100

    Herencia

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/inheritance.js
@@ -0,0 +1,38 @@
+// Clases
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    formatProduct() {
+        return `El producto ${this.name} tiene un precio de ${this.price}€`;
+    }
+
+    getProductPrice() {
+        return `El precio del producto ${this.name} es: ${this.price}€`;
+    }
+}
+
+const product1 = new Product('Monitor Curvo de 49', 800);
+const product2 = new Product('Monitor QHD 35', 300);
+const product3 = new Product('Televisión QHD 85', 900);
+
+class Book {
+    constructor(name, price, isbn) {
+        this.name = name;
+        this.price = price;
+        this.isbn = isbn;
+    }
+
+    getBookPrice() {
+        return this.price + '€';
+    }
+}
+
+book1 = new Book('JavaScript la Revolución', 45, '2232323452352387387423');
+
+console.log(product1);
+console.log(product2);
+console.log(product3.getProductPrice());
+console.log(book1.getBookPrice());
