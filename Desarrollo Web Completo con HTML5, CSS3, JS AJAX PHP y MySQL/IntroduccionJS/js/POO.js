44ad3ad3064d7032a34875e9b5d0465867eda133
colidom <colidom@outlook.com>
2022-04-27 23:58:54 +0100

    Formateo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/POO.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/POO.js
@@ -6,19 +6,18 @@ class Product {
     }
 
     formatProduct() {
-        return `El producto ${this.name} tiene un precio de ${this.price}€`
+        return `El producto ${this.name} tiene un precio de ${this.price}€`;
     }
 
     getProductPrice() {
-        return `El precio del producto ${this.name} es: ${this.price}€`
+        return `El precio del producto ${this.name} es: ${this.price}€`;
     }
 }
 
-const product1 = new Product('Monitor Curvo de 49', 800)
-const product2 = new Product('Monitor QHD 35', 300)
-const product3 = new Product('Televisión QHD 85', 900)
+const product1 = new Product('Monitor Curvo de 49', 800);
+const product2 = new Product('Monitor QHD 35', 300);
+const product3 = new Product('Televisión QHD 85', 900);
 
 console.log(product1);
 console.log(product2);
 console.log(product3.getProductPrice());
-
