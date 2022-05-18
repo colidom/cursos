ef8b3fb45658decf05d86a3a5103d544689c7dbd
colidom <colidom@outlook.com>
2022-05-18 23:01:23 +0100

    Qué es try catch

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/catch.js
@@ -0,0 +1,11 @@
+// ¿Qué es try catch?
+const num1 = 20;
+const num2 = 30;
+
+try {
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+} catch (error) {
+    console.log(error);
+}
