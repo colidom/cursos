b4ed5e2ea4568fb4e7bb0501ff382ab8b34faf6d
colidom <colidom@outlook.com>
2022-05-19 08:16:03 +0100

    Formateo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/property_methods.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/property_methods.js
@@ -1,24 +1,24 @@
 const player = {
-    play : function(id){
+    play: function (id) {
         console.log(`Reproduciendo canción con el ID: ${id}`);
     },
-    pause : function() {
+    pause: function () {
         console.log(`Pausando...`);
     },
-    createPlaylist : function(name) {
+    createPlaylist: function (name) {
         console.log(`Creando la playlist: ${name}`);
     },
-    playPlaylist : function(name) {
+    playPlaylist: function (name) {
         console.log(`Reproduciendo la playlist: ${name}`);
     }
-}
+};
 
-player.deleteSong = function(id) {
+player.deleteSong = function (id) {
     console.log(`Eliminando la canción: ${id}`);
-}
+};
 
 console.log(player.play(3840));
 console.log(player.pause());
 console.log(player.deleteSong(360));
-console.log(player.createPlaylist("Heavy Metal"));
-console.log(player.playPlaylist("Heavy Metal"));
+console.log(player.createPlaylist('Heavy Metal'));
+console.log(player.playPlaylist('Heavy Metal'));
