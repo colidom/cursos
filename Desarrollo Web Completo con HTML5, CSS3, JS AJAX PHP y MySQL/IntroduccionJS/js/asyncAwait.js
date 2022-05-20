3c39fbadb6fafe79799e514d67de840e681cf299
colidom <colidom@outlook.com>
2022-05-20 23:23:54 +0100

    Como trabajar con dos consultas async await?

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/asyncAwait.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/asyncAwait.js
@@ -10,15 +10,31 @@ function downloadNewCustomers() {
     });
 }
 
+function downloadLastOrders() {
+    return new Promise((resolve) => {
+        console.log('Descargando pedidos... espere...');
+
+        setTimeout(() => {
+            resolve('Los pedidos se han descargado');
+        }, 3000);
+    });
+}
+
 async function app() {
     try {
-        const result = await downloadNewCustomers();
-        console.log(result);
+        /*const customers = await downloadNewCustomers();
+        const orders = await downloadLastOrders();
+        console.log(customers);
+        console.log(orders); */
+        const result = await Promise.all([
+            downloadNewCustomers(),
+            downloadLastOrders()
+        ]);
+        console.log(result[0]);
+        console.log(result[1]);
     } catch (error) {
         console.log(error);
     }
 }
 
 app();
-
-console.log('Este código no se bloquea');
