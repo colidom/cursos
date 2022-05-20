59ea3c3230dc2ac0b4efa167e808188ff7c250a0
colidom <colidom@outlook.com>
2022-05-20 23:53:52 +0100

    Fetch API

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/fetchAPI.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/fetchAPI.js
@@ -4,7 +4,18 @@ function getEmproyees() {
     fetch(file)
         .then((result) => result.json())
 
-        .then((data) => console.log(data));
+        .then((data) => {
+            /* console.log(data.employees) */
+
+            const { employees } = data;
+            console.log(employees);
+
+            /* employees.forEach((employee) => {
+                console.log(employee.id);
+                console.log(employee.name);
+                console.log(employee.position);
+            }); */
+        });
 }
 
 getEmproyees();
