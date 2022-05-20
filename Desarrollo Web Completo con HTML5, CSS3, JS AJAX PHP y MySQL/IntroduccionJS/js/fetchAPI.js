eac499bf6e045cbf7a33590d72eaf09e0bfa7c2c
colidom <colidom@outlook.com>
2022-05-20 23:46:17 +0100

    Simplificando funciones

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/fetchAPI.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionJS/js/fetchAPI.js
@@ -2,12 +2,9 @@ function getEmproyees() {
     const file = 'employees.json';
 
     fetch(file)
-        .then((result) => {
-            return result.json();
-        })
-        .then((data) => {
-            console.log(data);
-        });
+        .then((result) => result.json())
+
+        .then((data) => console.log(data));
 }
 
 getEmproyees();
