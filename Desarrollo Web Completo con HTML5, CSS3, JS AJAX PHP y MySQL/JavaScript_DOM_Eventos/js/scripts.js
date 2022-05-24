63d9e1c8e68a1d4d6f2451ab311fdb7fb7c73744
colidom <colidom@outlook.com>
2022-05-24 23:54:22 +0100

    querySelector

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/JavaScript_DOM_Eventos/js/scripts.js
@@ -0,0 +1,8 @@
+// querySelector
+const heading = document.querySelector(".header__texto  h2"); // Retorna 0 o 1 elementos
+heading.textContent = "Nuevo heading";
+heading.classList.add("nueva-clase");
+console.log(heading);
+// querySelectorAll
+
+//getElementById
