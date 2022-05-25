e9bdfffba912f42f55571412a4281679a63426a4
colidom <colidom@outlook.com>
2022-05-25 23:27:20 +0100

    Añadido router en urls.py

--- DjangoRestFramework/profiles_api/urls.py
+++ DjangoRestFramework/profiles_api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello_viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
