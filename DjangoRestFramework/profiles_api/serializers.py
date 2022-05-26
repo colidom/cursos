4aee1983eb2e321598e954fa21bae34231685a18
colidom <colidom@outlook.com>
2022-05-26 23:06:20 +0100

    Creada clase UserProfileSerializer y class Meta

--- DjangoRestFramework/profiles_api/serializers.py
+++ DjangoRestFramework/profiles_api/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
+from profiles_api import models
+
 
 class HelloSerializers(serializers.Serializer):
     """Serializa un campo para probar nuestro APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """ Serializa objeto de perfil de usuario """
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
