b00c203556782ce127128be3b68fab2f9248bedc
colidom <colidom@outlook.com>
2022-06-02 23:42:25 +0100

    Añadido filtro para buscar usuarios por email o name

--- DjangoRestFramework/profiles_api/views.py
+++ DjangoRestFramework/profiles_api/views.py
@@ -2,7 +2,7 @@ from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 
 from profiles_api import serializers, models, permissions
 
@@ -101,3 +101,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
