39f8fe291b9cdc314ead6705494ee3ba9762194e
colidom <colidom@outlook.com>
2022-06-02 23:50:21 +0100

    Añadida clase de login de usuario

--- DjangoRestFramework/profiles_api/urls.py
+++ DjangoRestFramework/profiles_api/urls.py
@@ -9,5 +9,6 @@ router.register('profiles', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- DjangoRestFramework/profiles_api/views.py
+++ DjangoRestFramework/profiles_api/views.py
@@ -1,8 +1,10 @@
 from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import status, viewsets, filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers, models, permissions
 
@@ -103,3 +105,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Crea los tokens de autenticación de usuario """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
