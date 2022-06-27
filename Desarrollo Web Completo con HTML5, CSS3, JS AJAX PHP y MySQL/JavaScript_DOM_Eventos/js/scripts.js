9a96bdb06b4150d74e775171d0d9d1b390706139
colidom <colidom@outlook.com>
2022-06-27 22:23:21 +0100

    Seleccionar Elementos con querySelectorAll

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/JavaScript_DOM_Eventos/js/scripts.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/JavaScript_DOM_Eventos/js/scripts.js
@@ -1,8 +1,16 @@
 // querySelector
-const heading = document.querySelector(".header__texto  h2"); // Retorna 0 o 1 elementos
-heading.textContent = "Nuevo heading";
-heading.classList.add("nueva-clase");
+const heading = document.querySelector('.header__texto  h2'); // Retorna 0 o 1 elementos
+heading.textContent = 'Nuevo heading';
+heading.classList.add('nueva-clase');
 console.log(heading);
+
 // querySelectorAll
+const links = document.querySelectorAll('.navegacion a');
+console.log(links);
+links[0].textContent = 'Nuevo texto para Enlace';
+links[0].classList.add('nueva-clase');
+links[0].classList.remove('navegacion__enlace');
 
 //getElementById
+const heading2 = document.getElementById('heading');
+console.log(heading2);
