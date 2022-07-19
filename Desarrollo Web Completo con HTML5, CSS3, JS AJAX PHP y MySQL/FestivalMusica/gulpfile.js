a449631f11786031106e4671c66c043765068464
colidom <colidom@outlook.com>
2022-07-19 23:27:06 +0100

    Corregido require de sass

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -1,5 +1,5 @@
 const { src, dest } = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
   src("src/scss/app.scss") // Identificar el archivo SASS
