bb9bf3e4740e2a01fe1c54db1266744fed8ec695
colidom <colidom@outlook.com>
2022-07-19 23:26:25 +0100

    Compilando SASS con Gulp

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -1,8 +1,12 @@
-function task(callback) {
-  console.log("Mi primera tarea");
+const { src, dest } = require("gulp");
+const sass = require("gulp-sass");
 
-  callback();
+function css(done) {
+  src("src/scss/app.scss") // Identificar el archivo SASS
+    .pipe(sass()) // Compilarlo
+    .pipe(dest("build/css")); // Almacenarlo en el disco duro
+
+  done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
 }
 
-// Así llamamos a una función en Node.js
-exports.task = task;
+exports.css = css;
