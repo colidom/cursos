457973f047c9722444b09c998021c1317ad947e1
colidom <colidom@outlook.com>
2022-08-09 23:39:00 +0100

    Modificamos gulpfile para recargar la página cuando guardamos cualquier archivo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
-  src("src/scss/app.scss") // Identificar el archivo SASS
+  src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(sass()) // Compilarlo
     .pipe(dest("build/css")); // Almacenarlo en el disco duro
 
