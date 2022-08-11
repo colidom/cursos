0865b4f119e7e84dcbc2638de672ab566c6b348b
colidom <colidom@outlook.com>
2022-08-11 23:39:25 +0100

    Importando plumber

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
+    .pipe(plumber()) // Evita que si hay errores se detenga el workflow
     .pipe(sass()) // Compilarlo
     .pipe(dest("build/css")); // Almacenarlo en el disco duro
 
