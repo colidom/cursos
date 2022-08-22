302e031dceadcc50140c9d7ad4f1ea008169eee1
colidom <colidom@outlook.com>
2022-08-22 01:22:04 +0100

    Se pasa formato al documento

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -1,37 +1,34 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch } = require('gulp');
 
 // Css
-const sass = require("gulp-sass")(require("sass"));
-const plumber = require("gulp-plumber");
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 // Imagenes
 const webp = require('gulp-webp');
 
 function css(done) {
-  src("src/scss/**/*.scss") // Identificar el archivo SASS
-    .pipe(plumber()) // Evita que si hay errores se detenga el workflow
-    .pipe(sass()) // Compilarlo
-    .pipe(dest("build/css")); // Almacenarlo en el disco duro
+    src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe(plumber()) // Evita que si hay errores se detenga el workflow
+        .pipe(sass()) // Compilarlo
+        .pipe(dest('build/css')); // Almacenarlo en el disco duro
 
-  done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
+    done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
 }
 
 function versionWebp(done) {
-
     const opciones = {
-        qry: 50
+        qry: 50,
     };
 
-    src('src/img/**/*.{png, jpg}')
-    .pipe(webp(opciones))
-    .pipe( dest('build/img'))
+    src('src/img/**/*.{png, jpg}').pipe(webp(opciones)).pipe(dest('build/img'));
 
     done();
 }
 
 function dev(done) {
-  watch("src/scss/**/*.scss", css);
-  done();
+    watch('src/scss/**/*.scss', css);
+    done();
 }
 
 exports.css = css;
