937eaa34930cde452bd86cfd77988f0c0daaba31
colidom <colidom@outlook.com>
2022-08-22 01:21:33 +0100

    Utilizando Gulp para crear imágenes Webp

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -1,6 +1,11 @@
 const { src, dest, watch } = require("gulp");
+
+// Css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const webp = require('gulp-webp');
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -11,10 +16,24 @@ function css(done) {
   done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
 }
 
+function versionWebp(done) {
+
+    const opciones = {
+        qry: 50
+    };
+
+    src('src/img/**/*.{png, jpg}')
+    .pipe(webp(opciones))
+    .pipe( dest('build/img'))
+
+    done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 
 exports.css = css;
+exports.versionWebp = versionWebp;
 exports.dev = dev;
