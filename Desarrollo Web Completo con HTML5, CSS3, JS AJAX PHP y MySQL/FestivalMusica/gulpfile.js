0bdf9be5ea6a3ee49fabb50d484a4fe884a0e0d8
colidom <colidom@outlook.com>
2022-08-22 01:35:32 +0100

    Aligerar imágenes con Gulp

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -5,6 +5,8 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 // Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -14,6 +16,17 @@ function css(done) {
         .pipe(dest('build/css')); // Almacenarlo en el disco duro
 
     done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
+}
+
+function imagenes () {
+    const opciones = {
+        optimizationLevel: 3
+    };
+
+    src('src/img/**/*.{png, jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done()
 }
 
 function versionWebp(done) {
@@ -32,5 +45,6 @@ function dev(done) {
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp, dev);
