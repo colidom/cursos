2efe6d9e6d089a717d75cee83206827c2d58910f
colidom <colidom@outlook.com>
2022-08-31 22:56:46 +0100

    Creando imágenes Avif

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
 
@@ -40,6 +41,17 @@ function versionWebp(done) {
     done();
 }
 
+function versionAvif(done) {
+    const opciones = {
+        quality: 50,
+    };
+
+    src('src/img/**/*.{png, jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
@@ -48,4 +60,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
