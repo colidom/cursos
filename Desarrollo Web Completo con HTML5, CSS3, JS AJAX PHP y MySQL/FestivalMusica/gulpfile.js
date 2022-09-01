61b4ed6aaa52b2d1062b4f1228731858f1cf534e
colidom <colidom@outlook.com>
2022-09-01 23:39:30 +0100

    FIXED: Bug que hacía que no se tomaran las fotos en formato jpg

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -24,7 +24,7 @@ function imagenes (done) {
         optimizationLevel: 3
     };
 
-    src('src/img/**/*.{png, jpg}')
+    src('src/img/**/*.{png,jpg}')
     .pipe(cache(imagemin(opciones)))
     .pipe(dest('build/img'))
     done()
@@ -35,7 +35,7 @@ function versionWebp(done) {
         quality: 50
     };
 
-    src('src/img/**/*.{png, jpg}')
+    src('src/img/**/*.{png,jpg}')
     .pipe(webp(opciones))
     .pipe(dest('build/img'));
     done();
@@ -46,7 +46,7 @@ function versionAvif(done) {
         quality: 50
     };
 
-    src('src/img/**/*.{png, jpg}')
+    src('src/img/**/*.{png,jpg}')
     .pipe(avif(opciones))
     .pipe(dest('build/img'));
     done();
