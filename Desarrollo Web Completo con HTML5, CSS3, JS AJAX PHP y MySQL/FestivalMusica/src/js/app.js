a43c67f315f9f1ad37125793b396aee93ee2b7cf
colidom <colidom@outlook.com>
2022-09-03 23:07:49 +0100

    Generando el HTML de la galería

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -8,5 +8,15 @@ function iniciarApp() {
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
-    galeria.textContent = 'Hola desde JS';
+
+    for (let i = 1; i<= 12; i++) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `;
+
+       galeria.appendChild(imagen);
+    }
 }
