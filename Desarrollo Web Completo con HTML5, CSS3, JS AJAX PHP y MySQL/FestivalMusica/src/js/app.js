da3b8e9542e9540c9a8e75731d0927a0ddbb0a2c
colidom <colidom@outlook.com>
2022-09-03 23:26:49 +0100

    Detectando la imagen a la que le damos click para mostrar una versión más grande

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -16,7 +16,26 @@ function crearGaleria() {
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
         `;
+        imagen.onclick = function (){ // Hacemos un callback
+            mostrarImagen(i);
+        }
 
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+    `;
+
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
 }
