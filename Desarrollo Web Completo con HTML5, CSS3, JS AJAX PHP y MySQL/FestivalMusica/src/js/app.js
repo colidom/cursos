6daafc48041d54f1e83aee6a5e9931a5243b02bd
colidom <colidom@outlook.com>
2022-09-03 23:37:38 +0100

    Añadir un botón para cerrar la imagen

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -32,10 +32,22 @@ function mostrarImagen(id) {
         <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
     `;
 
+    // Crea el Overlay con la imagen
     const overlay = document.createElement("DIV");
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
 
+    // Botón para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function () {
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    // Añade el Overlay al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
