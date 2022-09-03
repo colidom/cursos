bee3cb85a63d75142b8fd6b59a2bc658ddbf7f13
colidom <colidom@outlook.com>
2022-09-03 23:00:35 +0100

    Primeros pasos con la Galería :Añadidas funciones JavaScript

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -1,1 +1,12 @@
-alert("Hola Mundo");
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+    galeria.textContent = 'Hola desde JS';
+}
