e4f61945481eed9332ca1519a1db5720fc0bd6f4
colidom <colidom@outlook.com>
2022-09-05 23:40:33 +0100

    Añadiendo un efecto Smooth scroll

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function iniciarApp() {
     crearGaleria();
+    scrollNav();
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    });
 }
 
 function crearGaleria() {
