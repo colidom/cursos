0306e641532daaace7764f855fb2c93261120b2f
colidom <colidom@outlook.com>
2022-09-06 23:35:53 +0100

    Fijar la barra al dar scroll

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/src/js/app.js
@@ -11,12 +11,15 @@ function iniciarApp() {
 function navegacionFija() {
     const barra = document.querySelector('.header');
     const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body')
 
     window.addEventListener('scroll', function() { 
         if (sobreFestival.getBoundingClientRect().bottom < 0) {
             barra.classList.add('fijo');
+            body.classList.add('body-scroll');
         } else {
             barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
         }
     });
 }
