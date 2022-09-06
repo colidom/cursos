037e5e534067eb507424a7cb48d30e3639432e0b
colidom <colidom@outlook.com>
2022-09-06 23:43:44 +0100

    Mejorar el performance del código con Gulp y CSS Nano

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/FestivalMusica/gulpfile.js
@@ -3,6 +3,9 @@ const { src, dest, watch, parallel } = require('gulp');
 // Css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -14,6 +17,7 @@ function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
         .pipe(plumber()) // Evita que si hay errores se detenga el workflow
         .pipe(sass()) // Compilarlo
+        .pipe(postcss([autoprefixer(), cssnano()])) 
         .pipe(dest('build/css')); // Almacenarlo en el disco duro
     done(); // Callback que avisa a Gulp que llegamos al final de la ejecución
 }
