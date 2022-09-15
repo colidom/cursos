38b463305942ff403e65afd62a512363068bde7a
colidom <colidom@outlook.com>
2022-09-15 21:57:56 +0100

    Variables y Constantes en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/02-variables.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/02-variables.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
 
+$nombre = "colidom";
+var_dump($nombre);
 
+define('constante', 'Este es el valor de la constante');
+var_dump(constante);
 
+const constante2 = "Hola 2";
+var_dump (constante2);
+
+// Convención nombres variables
+$nombreCliente = "Pedro";   // Más común en JS
+$nombre_cliente = "Pedro"; // Más común en PHP
 
 include 'includes/footer.php';
