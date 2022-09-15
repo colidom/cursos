bded42a410bdb16ea7644a6544d9f4364c43a0af
colidom <colidom@outlook.com>
2022-09-15 21:44:15 +0100

    Distintos tipos de print

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/01-hola-mundo.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/01-hola-mundo.php
@@ -1,9 +1,10 @@
 <?php
 
-echo "Hola mundo";
-
+echo "Hola mundo echo";
+print "Hola mundo print";
+print_r ("Hola mundo print");
 echo "<h1>Hola mundo PHP</h1>";
-
+var_dump("Hola mundo");
 ?>
 
 <h1>Hola mundo HTML</h1>
