76e75dc909abd1876decb82ba13186dd67edf48c
colidom <colidom@outlook.com>
2022-09-15 21:45:32 +0100

    Añadidos includes

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/01-hola-mundo.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/01-hola-mundo.php
@@ -1,10 +1,13 @@
 <?php
+include 'includes/header.php';
 
 echo "Hola mundo echo";
 print "Hola mundo print";
 print_r ("Hola mundo print");
 echo "<h1>Hola mundo PHP</h1>";
 var_dump("Hola mundo");
-?>
 
+
+include 'includes/footer.php';
+?>
 <h1>Hola mundo HTML</h1>
