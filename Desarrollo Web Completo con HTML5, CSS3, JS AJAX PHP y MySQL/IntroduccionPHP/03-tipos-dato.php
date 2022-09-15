e7b4e2cd04c1844e7fbe8c0a606464ec3d28256a
colidom <colidom@outlook.com>
2022-09-15 22:01:42 +0100

    Tipos de Dato en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/03-tipos-dato.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$logeado = false;
+var_dump($logeado);
 
+// Enteros
+$numero = 200;
+var_dump($numero);
 
+// Float
+$float = 200.5;
+var_dump($float);
+
+// String
+$string = "Colidom";
+var_dump($string);
+
+// Array
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
