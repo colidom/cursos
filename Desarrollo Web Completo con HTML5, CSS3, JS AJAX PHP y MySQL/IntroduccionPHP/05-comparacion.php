4841e1d6ef02e6faef35ef95f8c311c489117bf2
colidom <colidom@outlook.com>
2022-09-15 22:19:27 +0100

    Comparadores en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/05-comparacion.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/05-comparacion.php
@@ -3,7 +3,7 @@
 $numero1 = 20;
 $numero2 = 30;
 $numero3 = 40;
-$numero4 = "50";
+$numero4 = "40";
 
 // Mayor que 
 var_dump($numero1 > $numero2);
@@ -25,6 +25,10 @@ echo "</br>";
 var_dump($numero1 == $numero2);
 echo "</br>";
 
+// Igual que (tipo dato identical)
+var_dump($numero3 === $numero4);
+echo "</br>";
+
 // No igual que
 var_dump($numero1 != $numero2);
 echo "</br>";
@@ -37,6 +41,9 @@ echo "</br>";
 var_dump($numero1 <> $numero2);
 echo "</br>";
 
+// No igual que (-1 si izquierda es menor, 0 si son iguales, 1 si izquierda es mayor)
+var_dump($numero1 <=> $numero2);
+echo "</br>";
 
 
 include 'includes/footer.php';
