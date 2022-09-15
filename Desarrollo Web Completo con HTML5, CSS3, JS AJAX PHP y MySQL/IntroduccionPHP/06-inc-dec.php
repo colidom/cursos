4e25e19cf7024d883c176db647861acf5a1ed7e8
colidom <colidom@outlook.com>
2022-09-15 22:22:32 +0100

    Incrementos y decrementos en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/06-inc-dec.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/06-inc-dec.php
@@ -1,6 +1,15 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+$numero2 = 30;
+var_dump($numero1);
+var_dump($numero2);
 
+$numero1++;
+var_dump($numero1);
+
+$numero2--;
+var_dump($numero2);
 
 
 include 'includes/footer.php';
