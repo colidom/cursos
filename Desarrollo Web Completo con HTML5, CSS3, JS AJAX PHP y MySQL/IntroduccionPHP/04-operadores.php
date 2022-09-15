98ac8aa8b6c0a1bab8333bd8fc7ec2ed10d3d83d
colidom <colidom@outlook.com>
2022-09-15 22:06:20 +0100

    Números y Operadores en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/04-operadores.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/04-operadores.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+// Sumar
+echo $numero1 + $numero2;
+echo "</br>";
 
+// Resta
+echo $numero1 - $numero2;
+echo "</br>";
+
+// Multiplicar
+echo $numero1 * $numero2;
+echo "</br>";
+
+// Dividir
+echo $numero1 / $numero2;
+echo "</br>";
+
+// Resto
+echo $numero1 % $numero2;
+echo "</br>";
+
+// Exponentiation
+echo $numero1 ** $numero2;
+echo "</br>";
 
 include 'includes/footer.php';
