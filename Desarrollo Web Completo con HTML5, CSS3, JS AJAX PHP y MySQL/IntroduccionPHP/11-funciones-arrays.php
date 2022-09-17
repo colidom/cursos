8e363455a5867368843461a6b64f1cb9d5c7ba81
colidom <colidom@outlook.com>
2022-09-17 23:12:56 +0100

    Revisar si un elemento existe en un array y otros métodos

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/11-funciones-arrays.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/11-funciones-arrays.php
@@ -1,7 +1,38 @@
 <?php include 'includes/header.php';
 
+// in_array - buscar elementos en un array
+$carrito = ['Tablet', 'Ordenador',  'Televisión'];
 
+var_dump(in_array('Tablet', $carrito));
+var_dump(in_array('Auriculares', $carrito));
 
+// Ordenar elementos de un array
+$numeros = array(1, 3, 4, 1, 2, 5);
+sort($numeros); // De menor a mayor
+rsort($numeros); // De mayor a menor
 
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+// Ordenar elementos de un array asociativo
+$cliente = [
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'Colidom'
+];
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+asort($cliente); // Ordena por valores (orden alfabético)
+arsort($cliente); // Ordena por valores (orden alfabético al revez, luego números)
+ksort($cliente); // Ordena por clave (orden alfabético)
+krsort($cliente); // Ordena por clave (orden alfabético al revez)
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
