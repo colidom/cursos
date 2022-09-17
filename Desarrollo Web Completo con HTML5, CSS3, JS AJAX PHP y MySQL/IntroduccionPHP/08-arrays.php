fe0a213c9b3ccfe63fd61f49a9c3802d4a60e9cb
colidom <colidom@outlook.com>
2022-09-17 22:49:15 +0100

    Arreglos y Funciones para Arreglos en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/08-arrays.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/08-arrays.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+// Forma 1 para declarar un array
+$carrito = array();
+// Forma 2 para declarar un array
+$carrito = ['Tablet', 'Televisión', 'Ordenador'];
 
+// Acceder a los elementos de un array
+echo $carrito[1];
 
+// Añadir un nuevo elemento al array en la posición 3 (índice)
+$carrito[3] = 'Nuevo producto...';
+
+// Añadir un elemento nuevo al final del array
+array_push($carrito, 'Auriculares');
+// Añadir un elemento nuevo al principio del array
+array_unshift($carrito, 'Smartwatch');
+
+// Util para ver los contenidos de un Array
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+$clientes = array('Cliente1', 'Cliente2', 'Cliente3');
+// Util para ver los contenidos de un Array
+echo "<pre>";
+var_dump($clientes);
+echo "</pre>";
+echo $clientes[1];
 
 include 'includes/footer.php';
