90412b1fed28bf3b6e5c8f0b37dbccc566a5e134
colidom <colidom@outlook.com>
2022-09-17 22:56:07 +0100

    Arreglos asociativos o Associative Arrays en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/09-arrays-assoc.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/09-arrays-assoc.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Colidom',
+    'saldo'=> 200,
+    'informacion' => [
+        'tipo' => 'premium',
+        'disponible' => 100
+    ]
+];
 
+// Util para ver los contenidos de un Array
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
+echo $cliente['nombre'] . ": ";
+echo $cliente['informacion']['tipo'];
+
+// Agregamos propiedad nueva (Si existe dicha propiedad, se sobreescribe)
+$cliente['codigo'] = 12042309409234;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
