478914ae2dd220615c47ecbcd3a7c7e1f3b23968
colidom <colidom@outlook.com>
2022-09-19 23:48:58 +0100

    while, do while y foreach

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/13-loops.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/13-loops.php
@@ -32,7 +32,7 @@ echo "</br>";
 * múltiplo de 5 imprimir Buzz
 * múltiplo de 3 y 5 imprimir FIZZBUZZ
 */
-for($i = 1; $i < 1000; $i++) {
+for($i = 1; $i < 100; $i++) {
     if ($i % 3 === 0 && $i % 5 === 0) {
         echo $i . "- FIZZBUZZ</br>";
     } elseif ($i % 3 === 0) {
@@ -42,6 +42,47 @@ for($i = 1; $i < 1000; $i++) {
     } else {
         echo $i . "</br>";
     }
+}
+
+// Diferente sintaxis permitida por PHP
+for($i = 1; $i < 100; $i++):
+    if ($i % 3 === 0 && $i % 5 === 0):
+        echo $i . "- FIZZBUZZ</br>";
+    elseif ($i % 3 === 0):
+        echo $i . "- Fizz</br>";
+    elseif ($i % 5 === 0):
+        echo $i . "- Buzz</br>";
+    else:
+        echo $i . "</br>";
+    endif;
+endfor;
+
+// For Each
+$clientes = array('Colidom', 'Juan', 'Karen');
+foreach ($clientes as $cliente ) {
+    echo $cliente . "</br>";
+}
+
+// Lo mismo pero con for
+for ($i = 0; $i < count($clientes); $i++) {
+    echo $clientes[$i] . "</br>";
+}
+
+// Array asociativo
+$cliente = [
+    'nombre' => 'Colidom',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+// Iterar valores del array
+foreach ($cliente as $valor ) {
+    echo $valor . "</br>";
+}
+
+// Iterar key y valores del array
+foreach ($cliente as $key => $valor ) {
+    echo $key. ': '. $valor . "</br>";
 }
 
 include 'includes/footer.php';
