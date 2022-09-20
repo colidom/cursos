28af9b6a1e7acff02229c540914151e28d4059de
colidom <colidom@outlook.com>
2022-09-20 21:11:47 +0100

    Named Parameters en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/15-funciones.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/15-funciones.php
@@ -3,10 +3,12 @@ declare(strict_types = 1);
 include 'includes/header.php';
 
 function sumar(int $num1 = null, int $num2 = null) {
+    echo $num1;
     echo $num1 + $num2;
 }
 
 /* sumar(12, "12"); */
-sumar(12, 12);
+sumar(num2: 12, num1: 22);
+sumar(num1: 12, num2: 22);
 
 include 'includes/footer.php';
