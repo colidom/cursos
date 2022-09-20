f38b5f818b1d2e6b2763a1a02623a4eaad346466
colidom <colidom@outlook.com>
2022-09-20 21:02:20 +0100

    Funciones en PHP

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/15-funciones.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/15-funciones.php
@@ -1,7 +1,12 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types = 1);
+include 'includes/header.php';
 
+function sumar(int $num1 = null, int $num2 = null) {
+    echo $num1 + $num2;
+}
 
-
-
+/* sumar(12, "12"); */
+sumar(12, 12);
 
 include 'includes/footer.php';
