74b57d8ae95249df07774b14e0e00f5b3cca2cae
colidom <colidom@outlook.com>
2022-09-20 21:29:10 +0100

    json_encode y json_decode

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/18.json.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/18.json.php
@@ -1,8 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
+echo "<pre>";
+var_dump($productos);
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); // Para mostrar acentos
+$json_array = json_decode($json);
 
-
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 include 'includes/footer.php';
