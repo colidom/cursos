7646c880c5aa534633a0fa40c2db0f544cfd3122
colidom <colidom@outlook.com>
2022-09-20 21:08:43 +0100

    Funciones que retornan valores

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/16-types.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/16-types.php
@@ -1,7 +1,17 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types = 1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado) : string|int {
+    if($autenticado) {
+        return "El usuario está autenticado.";
+    } else {
+        return 20;
+    }
+}
 
+$usuario = usuarioAutenticado(false);
 
-
+echo $usuario;
 
 include 'includes/footer.php';
