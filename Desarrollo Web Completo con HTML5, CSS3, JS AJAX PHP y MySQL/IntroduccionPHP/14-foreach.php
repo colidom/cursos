b99ef9484e03284427da4aceb564bd7a00eef7b4
colidom <colidom@outlook.com>
2022-09-20 20:43:59 +0100

    foreach y mostrar el contenido de un array

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/14-foreach.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/14-foreach.php
@@ -1,6 +1,48 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
+// HTML en PHP
+/* foreach($productos as $producto) {
+    echo "<li>";
+    var_dump("Título del producto: " . $producto['nombre']);
+    echo "</li>";
+} */
 
+//PHP en HTML
+foreach($productos as $producto) { ?>
+   <li>
+        <p>Producto: <?php echo $producto['nombre'] ?></p>
+        <p>Precio: <?php echo $producto['precio'] ?>€</p>
+        <!-- Operador ternario -->
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible'; ?></p>
+        <!-- Fin Operador ternario -->
+        <?php 
+            if($producto['disponible']) {
+                echo "<p>Disponible</p>";
+            } else {
+                echo "<p>No disponible</p>";
+            }
+        ?>
+   </li>
+    <?php
+
+}
 
 include 'includes/footer.php';
