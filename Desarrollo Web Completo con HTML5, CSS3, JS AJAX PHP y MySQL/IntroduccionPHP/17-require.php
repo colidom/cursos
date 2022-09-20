9972a25599d759bc82ebc3e9c10f72048e60c02a
colidom <colidom@outlook.com>
2022-09-20 21:23:24 +0100

    Include y require

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/17-require.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/17-require.php
@@ -1,5 +1,13 @@
-<?php include 'includes/header.php';
+<?php
+// Distintas formas de include
+include 'includes/header.php'; // Recomendado para incluir ficheros como tpl, css
+include ('includes/header.php'); 
+require 'includes/header.php'; // Recomendado para incluir funciones, clases
+require ('includes/header.php');
+require_once 'includes/header.php'; // Si ya se ha incluido, no lo incluye nuevamente
+require_once 'funciones.php';
 
+iniciarApp();
 
 
 
---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHP/funciones.php
@@ -0,0 +1,5 @@
+<?php
+
+function iniciarApp() {
+    echo "Iniciando app";
+}
