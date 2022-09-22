17d40e088974b5c323315d117afc6632652d440e
colidom <colidom@outlook.com>
2022-09-22 20:14:32 +0100

    Formateo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/AppSalon_PHP/includes/funciones.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/AppSalon_PHP/includes/funciones.php
@@ -9,10 +9,10 @@ function obtenerServicios(): array
         $consulta = "SELECT * FROM servicios";
         $consulta = mysqli_query($db, $consulta);
 
-
         echo "<pre>";
         var_dump(mysqli_fetch_assoc($consulta)); // fetch_all nos retorna todo // fetch_array fetch_assoc
         echo "</pre>";
+
         $i = 0;
         $servicios = [];
         while ($row = mysqli_fetch_assoc($consulta)) {
