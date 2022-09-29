b7fc0d330eef162dbe2755082f65b3637d98b04a
colidom <colidom@outlook.com>
2022-09-29 01:09:03 +0100

    Añadido listener para cambiar a darkmode automáticamente cuando se cambia al modo oscuro del sistema

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/src/js/app.js
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/src/js/app.js
@@ -14,6 +14,14 @@ function  darkMode() {
         document.body.classList.remove('dark-mode');
     }
 
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
