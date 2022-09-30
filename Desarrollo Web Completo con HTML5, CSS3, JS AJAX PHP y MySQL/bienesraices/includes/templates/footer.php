468b93a2e2530bb45e2cc80643e79f769ddc063f
colidom <colidom@outlook.com>
2022-10-01 00:57:27 +0100

    Creando un Footer

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/includes/templates/footer.php
@@ -0,0 +1,15 @@
+        <footer class="footer seccion">
+            <div class="contenedor contenedor-footer">
+                <div class="navegacion">
+                    <a href="nosotros.php">Nosotros</a>
+                    <a href="anuncios.php">Anuncios</a>
+                    <a href="blog.php">Blog</a>
+                    <a href="contacto.php">Contacto</a>
+                </div>
+            </div>
+            <p class="copyright">Todos los Derechos Reservados <?php echo date('Y'); ?> &copy</p>
+        </footer>
+
+        <script src="build/js/bundle.min.js"></script>
+    </body>
+</html>
--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/nosotros.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/nosotros.php
@@ -44,18 +44,6 @@
         </div>
     </section>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <div class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </div>
-        </div>
-        <p class="copyright">Todos los Derechos Reservados 2021 &copy</p>
-    </footer>
-
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php 
+    include 'includes/templates/footer.php';
+?>
