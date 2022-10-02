340f5c403151743e7df3ca9b42a7ed097b3c23d2
colidom <colidom@outlook.com>
2022-10-02 23:41:44 +0100

    Creando nuestra primer propiedad en la base de datos

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/propiedades/crear.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/propiedades/crear.php
@@ -7,6 +7,26 @@
         echo "<pre>";
         var_dump($_POST);
         echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedorId = $_POST['vendedor'];
+
+        // Insertar en la base de datos
+        $query = "INSERT INTO propiedades ( titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId)
+                VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId' ) ";
+
+        // echo $query;
+
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            echo "Insertado registro correctamente en la DB";
+        }
     }
 
     require '../../includes/funciones.php';
