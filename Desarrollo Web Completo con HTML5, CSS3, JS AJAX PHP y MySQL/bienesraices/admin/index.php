1f0269569d369d06a55260ad73fea647153f4a58
colidom <colidom@outlook.com>
2022-10-05 22:27:37 +0100

    Creando una tabla para listar las propiedades de la base de datos

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
@@ -12,6 +12,30 @@
             <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
         <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
         <a href="/admin/propiedades/borrar.php" class="boton boton-verde">Borrar propiedad</a>
+    
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en el monte</td>
+                    <td><img src="/imagenes/0ea6f6f376046518aa392a3bc814f179.jpg" class="imagen-tabla"></td>
+                    <td>75.000€</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php 
