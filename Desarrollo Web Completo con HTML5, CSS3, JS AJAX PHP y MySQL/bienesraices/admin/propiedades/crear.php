0f7eb525f1dd299b2921ff065e4ae834830b7c09
colidom <colidom@outlook.com>
2022-10-05 22:02:54 +0100

    Generando Nombres únicos para las imágenes

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/propiedades/crear.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/propiedades/crear.php
@@ -75,19 +75,20 @@
 
             /* Subida de archivos */
             // Crear carpeta
-            $carpetaImagenes = '../../imagenes';
+            $carpetaImagenes = '../../imagenes/';
             if (!is_dir($carpetaImagenes)) {
                 mkdir($carpetaImagenes);
             }
 
-            // Subir imagen
-            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . "/archivo.jpg");
+            // Generar un nombre único
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg" ;
 
-            exit;
+            // Subir imagen
+            move_uploaded_file( $imagen['tmp_name'], $carpetaImagenes . $nombreImagen );
 
             // Insertar en la base de datos
-            $query = "INSERT INTO propiedades ( titulo, precio, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId)
-                    VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId' ) ";
+            $query = "INSERT INTO propiedades ( titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId)
+                    VALUES ('$titulo', '$precio', '$nombreImagen','$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId' ) ";
     
             // echo $query;
     
