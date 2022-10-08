f0ed676b377878b1807115cca2d629a0fe5663ba
colidom <colidom@outlook.com>
2022-10-08 23:48:20 +0100

    indentacion

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
@@ -22,8 +22,8 @@
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
             <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
-        <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
-        <a href="/admin/propiedades/borrar.php" class="boton boton-verde">Borrar propiedad</a>
+            <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
+            <a href="/admin/propiedades/borrar.php" class="boton boton-verde">Borrar propiedad</a>
     
         <table class="propiedades">
             <thead>
