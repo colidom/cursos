1ea705c971a5bb67b6f41f13bccf4e7d2079eb31
colidom <colidom@outlook.com>
2022-10-08 23:53:48 +0100

    formateo

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
@@ -21,10 +21,10 @@
         <?php if(intval($resultado) === 1): ?>
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
-            <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
-            <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
-            <a href="/admin/propiedades/borrar.php" class="boton boton-verde">Borrar propiedad</a>
-    
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+        <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
+        <a href="/admin/propiedades/borrar.php" class="boton boton-verde">Borrar propiedad</a>
+
         <table class="propiedades">
             <thead>
                 <tr>
