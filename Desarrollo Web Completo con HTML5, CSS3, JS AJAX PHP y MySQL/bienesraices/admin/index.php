dca0cbbd83e2514de1675b52e1f200dadcb2b098
colidom <colidom@outlook.com>
2022-10-10 00:06:15 +0100

    Eliminar Propiedades

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
@@ -11,6 +11,26 @@
     // Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if ($id) {
+
+            // Eliminar el archivo
+            $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+            $resultado = mysqli_query($db, $query);
+            $propiedad = mysqli_fetch_assoc($resultado);
+            unlink('../imagenes/' . $propiedad['imagen']);
+            // Eliminar la propiedad en la DB
+            $query = "DELETE FROM propiedades WHERE id = ${id}";
+            $resultado = mysqli_query($db, $query);
+            if ($resultado) {
+                header('location: /admin');
+            }
+        }
+    }
+
     // Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -45,7 +65,10 @@
                     <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"></td>
                     <td><?php echo $propiedad['precio']; ?>€</td>
                     <td>
-                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id'] ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
                         <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
