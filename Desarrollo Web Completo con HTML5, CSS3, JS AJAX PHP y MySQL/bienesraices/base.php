c79322988bc8dd20a7484d20fd50784a52a0e891
colidom <colidom@outlook.com>
2022-10-12 22:10:36 +0100

    Añadido base.php

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/base.php
@@ -0,0 +1,12 @@
+<?php 
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Título Página</h1>
+</main>
+
+<?php 
+    incluirTemplate('footer');
+?>
