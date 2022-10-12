3db115dedbb0204e4d7a3526cf5b9e31341d0bd5
colidom <colidom@outlook.com>
2022-10-12 22:36:14 +0100

    Validaciones al Formulario

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/login.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/login.php
@@ -1,4 +1,25 @@
-<?php 
+<?php
+    // Añadir conexión a db
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    $errores = [];
+
+    // Autenticar el usuario
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if (!$email) {
+            $errores[] = "El email es obligatorio o no es válido";
+        }
+        if (!$password) {
+            $errores[] = "El password es obligatorio o no es válido";
+        }
+    }
+
+    // Incluye el header
     require 'includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -6,15 +27,21 @@
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
 
-    <form action="" class="formulario">
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <form method="POST" class="formulario">
         <fieldset>
             <legend>Información Personal</legend>
 
             <label for="email">E-mail</label>
-            <input type="email" placeholder="Su Email" id="email">
+            <input type="email" name="email" placeholder="Su Email" id="email">
             
             <label for="password">Contraseña</label>
-            <input type="password" placeholder="Su password" id="password">
+            <input type="password" name="password" placeholder="Su password" id="password">
         </fieldset>
         <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
     </form>
