50660a676c8087250ae74172214183863c411981
colidom <colidom@outlook.com>
2022-10-12 22:07:43 +0100

    Hashear Passwords

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/usuario.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/usuario.php
@@ -6,10 +6,13 @@ $db = conectarDB();
 // Crear un email y password
 $email = "colidom@outlook.com";
 $password = "123456";
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
+var_dump($passwordHash);
 // Query para crear el usuario
-$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${password}'); ";
+$query = "INSERT INTO usuarios (email, password) VALUES ('${email}', '${passwordHash}'); ";
 
-echo $query;
+// echo $query;
+
 // Agregarlo a la DB
 mysqli_query($db, $query);
