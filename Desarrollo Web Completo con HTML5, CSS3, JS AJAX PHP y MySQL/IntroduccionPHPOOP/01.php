1aebca4f824dd02a3d186400bd59415984814225
colidom <colidom@outlook.com>
2022-10-13 20:00:31 +0100

    Creando una Clase e instanciandola

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/01.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/01.php
@@ -1,4 +1,30 @@
 <?php include 'includes/header.php';
 
+// Definir una clase
+class Producto {
+
+    public $nombre;
+    public $precio;
+    public $disponible;
+
+}
+
+$producto = new Producto();
+$producto->nombre = "Tablet"; 
+$producto->precio = 499; 
+$producto->disponible = true; 
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto();
+$producto2->nombre = "Monitor"; 
+$producto2->precio = 699; 
+$producto2->disponible = true; 
+
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
 
 include 'includes/footer.php';
