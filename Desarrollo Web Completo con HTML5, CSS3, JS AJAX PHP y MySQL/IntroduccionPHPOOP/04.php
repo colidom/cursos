f6160341de33d928d0af5b5f7eed532d98c7c58a
colidom <colidom@outlook.com>
2022-10-14 00:05:37 +0100

    Herencia de Classes

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/04.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/04.php
@@ -1,4 +1,51 @@
 <?php include 'includes/header.php';
 
+class Transporte {
+
+    public function __construct(protected int $ruedas, protected int $capacidad) {
+        
+    }
+
+    public function getInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas ";
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+    }
+
+}
+
+class Bicicleta extends Transporte {
+    
+    /* Sobreescribir método al declararlo con el mismo nombre que en la clase padre(Transporte) */
+    public function getInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas y no gasta gasolina";
+    }
+}
+
+class Automovil extends Transporte {
+
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision) {
+        
+    }
+
+    public function getTransmision() : string {
+        return $this->transmision;
+    }
+
+}
+
+$bicicleta = new Bicicleta(2, 1);
+echo $bicicleta->getInfo();
+echo $bicicleta->getRuedas();
+/* echo $bicicleta->getInformacion(); */
+echo "<hr>";
+
+$auto = new Automovil(4, 5, "Manual");
+echo $auto->getInfo();
+echo $auto->getRuedas();
+echo $auto->getTransmision();
+echo "<hr>";
 
 include 'includes/footer.php';
