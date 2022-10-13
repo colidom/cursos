97c631d81872293a0275a65986f04ca3fa2c6e23
colidom <colidom@outlook.com>
2022-10-13 19:49:51 +0100

    Añadidos recursos PHP POO

---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/02.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php';
+
+
+include 'includes/footer.php';
---
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducción a PHP - Código Con Juan</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <div class="contenido">
+        <h1>Desarrollo Web Completo</h1>
+        <a href="http://www.codigoconjuan.com" target="_blank">codigoconjuan.com</a>
+        <p>Introducción a Programación Orientada a Objetos en PHP</p>
+        <p class="hashtag">#DesarrolloWebCompleto</p>
+
+        <div class="resultado">
