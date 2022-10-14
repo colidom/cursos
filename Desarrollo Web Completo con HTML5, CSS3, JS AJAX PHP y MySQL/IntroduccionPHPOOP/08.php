9f7d41b0bb63fcf675c581d88f426a4058d384ac
colidom <colidom@outlook.com>
2022-10-14 23:49:32 +0100

    Autoload con Composer

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/08.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/08.php
@@ -1,17 +1,9 @@
 <?php include 'includes/header.php';
 
+require 'vendor/autoload.php';
+
 use App\Clientes;
 use App\Detalles;
-
-// Autoload de Classes
-function mi_autoload($clase) {
-
-    $partes = explode('\\', $clase);
-
-    require __DIR__ . '/clases/' . $partes[1] .'.php';
-}
-
-spl_autoload_register('mi_autoload');
 
 $clientes = new Clientes();
 echo "<br>";
