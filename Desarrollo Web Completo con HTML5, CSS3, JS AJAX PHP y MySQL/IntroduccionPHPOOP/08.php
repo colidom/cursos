2ae7ad0587910c8dc928698e7994c7de7b5bded3
colidom <colidom@outlook.com>
2022-10-14 22:53:38 +0100

    Quito código comentado

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/08.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/08.php
@@ -1,9 +1,5 @@
 <?php include 'includes/header.php';
 
-// Incluir las clases
-/* require 'clases/Clientes.php';
-require 'clases/Detalles.php'; */
-
 // Autoload de Classes
 function mi_autoload($clase) {
     require __DIR__ . '/clases/' . $clase .'.php';
