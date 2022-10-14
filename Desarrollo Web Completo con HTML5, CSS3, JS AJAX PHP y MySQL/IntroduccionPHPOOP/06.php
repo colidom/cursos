ab4b1e21d14c2d4f7e0dfa045257f8cc7d93b88a
colidom <colidom@outlook.com>
2022-10-14 22:17:59 +0100

    Interfaces

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/06.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/06.php
@@ -1,4 +1,24 @@
 <?php include 'includes/header.php';
 
+interface TransporteInterfaz {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+class Transporte implements TransporteInterfaz {
+
+    public function __construct(protected int $ruedas, protected int $capacidad) {
+        
+    }
+
+    public function getInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas ";
+    }
+
+    public function getRuedas() : int {
+        return $this->ruedas;
+    }
+
+}
 
 include 'includes/footer.php';
