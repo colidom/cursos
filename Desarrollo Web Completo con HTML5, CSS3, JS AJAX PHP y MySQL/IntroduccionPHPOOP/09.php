7b458964c582ca92993b0493f633ea571d56db2b
colidom <colidom@outlook.com>
2022-10-15 01:16:47 +0100

    Consultar la Base de datos con POO y MySQLi

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/09.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/09.php
@@ -1,4 +1,25 @@
 <?php include 'includes/header.php';
 
+// Conectar a la DB con Mysqli POO
+$db = new mysqli('localhost', 'colidom', 'Mysql2021', 'bienesraices_crud');
+
+// Creamos el query
+$query = "SELECT titulo, imagen FROM propiedades";
+
+// Lo preparamos
+$stmt = $db->prepare($query);
+
+// Lo ejecutamos
+$stmt->execute();
+
+// Creamos la variable
+$stmt->bind_result($titulo, $imagen);
+
+// Asignamos el resultado
+$stmt->fetch();
+
+// Imprimimos el resultado
+var_dump($titulo);
+var_dump($imagen);
 
 include 'includes/footer.php';
