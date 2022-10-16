31441d0604368f13b75b04c9c68046facd6d2708
colidom <colidom@outlook.com>
2022-10-16 21:15:58 +0100

    Consultar la Base de datos con POO y PDO

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/10.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/IntroduccionPHPOOP/10.php
@@ -1,4 +1,28 @@
 <?php include 'includes/header.php';
 
+$host = 'localhost';
+$db = "bienesraices_crud";
+$user = 'colidom';
+$pass = 'Mysql2021';
+
+// Conectar a la DB con PDO
+$db = new PDO('mysql:host=$host;dbname=$db', $user, $pass);
+
+// Creamos el query
+$query = "SELECT titulo FROM propiedades";
+
+// Lo ejecutamos
+$stmt->execute();
+
+// Obtener los resultados
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Iterar los resultados
+foreach($resultado as $propiedad):
+    echo $propiedad['titulo'];
+    echo "</br>";
+    echo $propiedad['imagen'];
+    echo "</br>";
+endforeach;
 
 include 'includes/footer.php';
