89e334e36ba1fbe28329722642036e45f7f2c998
colidom <colidom@outlook.com>
2022-10-17 23:25:51 +0100

    Fixed espacio

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/classes/propiedad.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/classes/propiedad.php
@@ -6,6 +6,7 @@ class Propiedad {
 
     // Base de datos
     protected static $db;
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
 
     public $id;
     public $titulo;
@@ -17,6 +18,11 @@ class Propiedad {
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+
+    // Definir la conexión a la db
+    public static function setDB($database) {
+        self::$db = $database;
+    }
 
     public function __construct($args = [])
     {
@@ -33,6 +39,10 @@ class Propiedad {
     }
 
     public function guardar() {
+
+        // Sanitizar los datos
+        $atributos = $this->sanitizarAtributos();
+
         // Insertar en la base de datos
         $query = "INSERT INTO propiedades ( titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId)
         VALUES ('$this->titulo', '$this->precio', '$this->imagen','$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId' ) ";
@@ -42,8 +52,16 @@ class Propiedad {
         debuguear($resultado);
     }
 
-    // Definir la conexión a la db
-    public static function setDB($database) {
-        self::$db = $database;
+    public function atributos() {
+        $atributos = [];
+        foreach (self::$columnasDB as $columna) {
+            $atributos[$columna] = $this->columna;
+        }
+        return $atributos;
+    }
+
+    public function sanitizarAtributos() {
+        $atributos = $this->atributos();
+        debuguear($atributos);
     }
 }
