c8a384b5924b7f95e8c205babcb476cd5c7515f7
colidom <colidom@outlook.com>
2022-10-18 23:03:35 +0100

    Iterando sobre los resultados

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/admin/index.php
@@ -56,21 +56,21 @@
                 </tr>
             </thead>
             <tbody> <!-- Mostrar los resultados -->
-            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
-                <tr>
-                    <td><?php echo $propiedad['id']; ?></td>
-                    <td><?php echo $propiedad['titulo']; ?></td>
-                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"></td>
-                    <td><?php echo $propiedad['precio']; ?>€</td>
-                    <td>
-                        <form method="POST" class="w-100">
-                            <input type="hidden" name="id" value="<?php echo $propiedad['id'] ?>">
-                            <input type="submit" class="boton-rojo-block" value="Eliminar">
-                        </form>
-                        <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
-                    </td>
-                </tr>
-                <?php endwhile; ?>
+                <?php foreach($propiedades as $propiedad): ?>
+                    <tr>
+                        <td><?php echo $propiedad->id; ?></td>
+                        <td><?php echo $propiedad->titulo; ?></td>
+                        <td><img src="/imagenes/<?php echo $propiedad->imagen; ?>" class="imagen-tabla"></td>
+                        <td><?php echo $propiedad->precio; ?>€</td>
+                        <td>
+                            <form method="POST" class="w-100">
+                                <input type="hidden" name="id" value="<?php echo $propiedad->id ?>">
+                                <input type="submit" class="boton-rojo-block" value="Eliminar">
+                            </form>
+                            <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </main>
