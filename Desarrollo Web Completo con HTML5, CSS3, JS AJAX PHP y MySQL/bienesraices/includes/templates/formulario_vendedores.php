881fd2cc6e3c1d1a4c5839f8379f288c2ebb8ac0
colidom <colidom@outlook.com>
2022-10-28 01:10:30 +0100

    Fixed:: Informamos los datos del formulario correcttamente

--- Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/includes/templates/formulario_vendedores.php
+++ Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL/bienesraices/includes/templates/formulario_vendedores.php
@@ -5,7 +5,7 @@
     <input type="text" id="nombre" name="vendedor[nombre]" placeholder="Nombre vendedor" value="<?php echo __s($vendedor->nombre); ?>">
     
     <label for="apellido">Apellido:</label>
-    <input type="text" id="apellido" name="vendedor[apellido]" placeholder="Apellido vendedor" value="<?php echo __s($vendedor->nombre); ?>">
+    <input type="text" id="apellido" name="vendedor[apellido]" placeholder="Apellido vendedor" value="<?php echo __s($vendedor->apellido); ?>">
     
 </fieldset>
 
@@ -13,6 +13,6 @@
     <legend>Información extra</legend>
 
     <label for="telefono">Telefono:</label>
-    <input type="tel" id="telefono" name="vendedor[telefono]" placeholder="Telefono del vendedor" value="<?php echo __s($vendedor->nombre); ?>">
+    <input type="tel" id="telefono" name="vendedor[telefono]" placeholder="Telefono del vendedor" value="<?php echo __s($vendedor->telefono); ?>">
     
 </fieldset>
