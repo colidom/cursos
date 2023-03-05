fe83c93896e57982364214e2ba27cedc2d895ba0
colidom <colidom@outlook.com>
2023-03-05 21:20:03 +0000

    Conservamos comillas dobles y simples, evitando que el auto formateador las cambie

--- python_desde_0_mouredev/00_hello.py
+++ python_desde_0_mouredev/00_hello.py
@@ -1,19 +1,19 @@
 # Esto es un comentario:
 # Nuestro Hola Mundo en Python
 print("Hola Mundo")
-print("Hola Python")
+print('Hola Python')
 
-print(type("Hola Python"))
+print(type('Hola Python'))
 
-
-""" 
-Este es un 
-comentario en 
-varias línea(doc string)
-"""
 
 """ 
 Este es un 
 comentario en 
 varias línea(doc string)
 """
+
+''' 
+Este es un 
+comentario en 
+varias línea(doc string)
+'''
