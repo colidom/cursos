602c6760e83dd22af63a040f3b0096e76732095e
colidom <colidom@outlook.com>
2023-05-28 02:33:09 +0100

    PHP en HTML o HTML en PHP

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/08_html.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/08_html.php
@@ -1,3 +1,18 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+$estudiante = array(
+  'nombre' => 'Carlos',
+  'apellido' => 'Oliva',
+  'username' => 'colidom',
+  'pais' => 'España',
+  'edad' => 32,
+  'sexo' => 'M'
+);
+
+echo "<h1>Hola! " . $estudiante['nombre'] . " </h1>";
+?>
+
+<h1>Hola! <?php echo $estudiante['nombre']; ?></h1>
