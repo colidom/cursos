9909c967692d16d07c0f07e40773739fe32176f2
colidom <colidom@outlook.com>
2023-05-28 02:24:42 +0100

    Arrays Asociativos

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/06_arrays_assoc.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/06_arrays_assoc.php
@@ -1,3 +1,19 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+$estudiante = array(
+  'nombre' => 'Carlos',
+  'apellido' => 'Oliva',
+  'username' => 'colidom',
+  'pais' => 'España',
+  'edad' => 32,
+  'sexo' => 'M'
+);
+
+echo "Estudiante: ";
+echo $estudiante['nombre'] . " " . $estudiante['apellido']  . " " . $estudiante['pais'];
+echo "<pre>";
+var_dump($estudiante);
+echo "</pre>";
