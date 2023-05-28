eb4e33b88159d444f38ef425e1d11d6540fc1149
colidom <colidom@outlook.com>
2023-05-28 02:02:41 +0100

    Crear y Definir Variables

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/03_variables.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/03_variables.php
@@ -1,3 +1,14 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
+?>
+
+<?php
+$nombre = "Colidom";
+$numero_favorito = 20;
+$autenticado = true;
+
+echo $nombre;
+echo "</br>";
+var_dump($nombre . ": " . $numero_favorito . " Auth: " . $autenticado);
+
 ?>
