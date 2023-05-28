f56222624d6c188647a8a985719eebf279e9a33c
colidom <colidom@outlook.com>
2023-05-28 03:29:10 +0100

    Classes, Objetos, Constructores y Métodos

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/17_class.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/17_class.php
@@ -1,3 +1,27 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+class Cliente
+{
+    public $nombre;
+
+    public function __construct($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function mostrar_nombre()
+    {
+        echo "El nombre es: {$this->nombre}";
+    }
+}
+
+# Instanciar clase
+$cliente = new Cliente("colidom");
+
+echo "<pre>";
+$cliente->mostrar_nombre();
+echo "</pre>";
