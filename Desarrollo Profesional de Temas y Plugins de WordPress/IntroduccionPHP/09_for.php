792544e652bbaeaf45421da8f78d08ec3ce5a2d1
colidom <colidom@outlook.com>
2023-05-28 02:41:08 +0100

    for Loop

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/09_for.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/09_for.php
@@ -1,3 +1,22 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+$estudiante = array(
+  'nombre' => 'Carlos',
+  'apellido' => 'Oliva',
+  'username' => 'colidom',
+  'pais' => 'España',
+  'edad' => 32,
+  'sexo' => 'M'
+);
+
+for ($i = 0; $i <= 100; $i++) {
+  if ($i == 10) {
+    echo "El número es 10 </br>";
+    break;
+  }
+  echo "Número: " . $i . "</br>";
+}
