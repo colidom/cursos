16092c744ed8d55d927770452d5cd91d19f3e060
colidom <colidom@outlook.com>
2023-05-28 03:11:10 +0100

    Funciones con Parametros

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/14_func_args.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/14_func_args.php
@@ -1,3 +1,23 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+<?php
+
+function saludar($nombre)
+{
+  echo "Hola {$nombre}! Bienvenidos al sitio web! </br>";
+}
+
+function sumar($n1 = 0, $n2 = 0)
+{
+  echo $n1 + $n2;
+}
+
+echo saludar("colidom");
+echo saludar("carlos");
+echo saludar("laura");
+
+echo sumar(1, 20);
+echo "</br>";
+echo sumar(100);
+0
