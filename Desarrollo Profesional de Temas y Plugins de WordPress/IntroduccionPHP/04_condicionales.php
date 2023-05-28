9b6c54dfe1e9ff1c47f2990a3618ed4c8bc1344b
colidom <colidom@outlook.com>
2023-05-28 02:09:02 +0100

    Escribir código condicional

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/04_condicionales.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/04_condicionales.php
@@ -1,3 +1,18 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
+?>
+
+<?php
+
+$num1 = 20;
+$num2 = 20;
+
+if ($num1 > $num2) {
+    echo "El número 2 es mayor";
+} else if ($num1 == $num2) {
+    echo "Ambos números son iguales";
+} else {
+    echo "El número 1 es mayor";
+}
+
 ?>
