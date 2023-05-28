47cb873a8d164d49c332002a2d0c1199ff47e27f
colidom <colidom@outlook.com>
2023-05-28 03:20:38 +0100

    Funciones que Retornan Valores

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/16_func_return.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/16_func_return.php
@@ -1,3 +1,26 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+function calcular_total($cantidad = 0)
+{
+    $total = $cantidad * 1.07;
+    return $total;
+}
+
+function pagar($total)
+{
+    $balance = 1000;
+
+    if ($total > $balance) {
+        echo "Saldo insuficiente!";
+    } else {
+        echo "Pago realizado correctamente!";
+    }
+}
+
+echo calcular_total(20);
+echo "</br>";
+echo pagar(100);
