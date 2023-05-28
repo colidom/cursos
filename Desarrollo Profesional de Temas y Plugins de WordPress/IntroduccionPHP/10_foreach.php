90d52d57bf04c2373821ae3884bcaa9fc8fdeda4
colidom <colidom@outlook.com>
2023-05-28 02:49:33 +0100

    foreach Loop

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/10_foreach.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/10_foreach.php
@@ -1,3 +1,31 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+$frutas = array('manzana', 'naranja', 'melón', 'uvas');
+$estudiante = array(
+  'nombre' => 'Carlos',
+  'apellido' => 'Oliva',
+  'username' => 'colidom',
+  'pais' => 'España',
+  'edad' => 32,
+  'sexo' => 'M'
+);
+
+echo "***for convencional*** </br>";
+
+for ($i = 0; $i < count($frutas); $i++) {
+  echo $frutas[$i] . "</br>";
+}
+
+echo "</br>***foreach***</br>";
+foreach ($frutas as $fruta) {
+  echo $fruta . "</br>";
+}
+
+echo "</br>***foreach key => value*** </br>";
+foreach ($estudiante as $clave => $valor) {
+  echo $clave . ": " . $valor . "</br>";
+}
