2bcdf7d7d840ff300df99d61da134ee2b8f9fdc2
colidom <colidom@outlook.com>
2023-05-28 03:02:32 +0100

    Librería Estandard en PHP

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/12_lib.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/12_lib.php
@@ -1,3 +1,27 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+$frutas = array('manzana', 'naranja', 'melón', 'uvas');
+echo "Número frutas: " . count($frutas);
+
+echo "</br>";
+
+$nombre_usuario = "               colidom          ";
+echo "Mi nombre usuario es: " . $nombre_usuario;
+echo "</br>";
+echo "Mi nombre usuario en mayúsculas es: " . strtoupper($nombre_usuario);
+echo "</br>";
+trim($nombre_usuario);
+echo "Mi nombre usuario en mayúsculas es: " . ($nombre_usuario);
+
+echo "</br>";
+
+$password = '          password         ';
+if (strlen(trim($password)) < 10) {
+  echo "La contraseña es muy débil";
+} else {
+  echo "La contraseña es muy fuerte";
+}
