3d4aea276a763f5008923c00514662015ba69e0b
colidom <colidom@outlook.com>
2023-05-28 02:19:49 +0100

    Arrays en PHP

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/05_arrays.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/05_arrays.php
@@ -1,3 +1,16 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
+?>
+
+<?php
+$frutas = array('manzana', 'naranja', 'melón', 'uvas');
+$frutas2 = ['manzana', 'naranja', 'melón', 'uvas'];
+
+array_push($frutas, 'pera');
+array_push($frutas, 'mango');
+
+echo "<pre>";
+var_dump($frutas2);
+echo "</pre>";
+
 ?>
