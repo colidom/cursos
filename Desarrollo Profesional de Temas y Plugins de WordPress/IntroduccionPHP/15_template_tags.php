44c6303bad0cf78f01b8c948da0db665efe564b9
colidom <colidom@outlook.com>
2023-05-28 03:14:11 +0100

    Template Tags

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/15_template_tags.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/15_template_tags.php
@@ -1,3 +1,28 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+
+function the_title()
+{
+  echo "Título de mi blog";
+}
+
+function the_date()
+{
+  echo date('Y m d');
+}
+
+function the_author()
+{
+  echo "Autor de mi blog";
+}
+
+?>
+
+<div class="entrada">
+  <h2><?php echo the_title(); ?></h2>
+  <p>Publicado el: <?php the_date(); ?></p>
+  <p>Escrito por: <?php the_author(); ?></p>
+</div>
