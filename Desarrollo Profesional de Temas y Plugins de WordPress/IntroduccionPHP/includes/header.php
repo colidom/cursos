9b8bee21e6627332dcbf497052d633cc52c1e704
colidom <colidom@outlook.com>
2023-05-28 01:46:22 +0100

    Añadidos materiales del curso

---
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP - Codigo Con Juan</title>
+    <link rel="stylesheet" href="dist/output.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body class="bg-slate-50">
+    <header class="bg-gray-800">
+            <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:flex lg:justify-between lg:px-8">
+                <div class="max-w-xl">
+                    <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">Aprendiendo PHP</h2>
+                    <p class="mt-3 text-xl text-gray-400">
+                        Introducción a PHP por Código Con Juan 
+                    </p>
+                    <p class="mt-3 text-xl text-gray-400">
+                        Desarrollo Profesional de Temas y Plugins de WordPress
+                    </p>
+                    <div class="flex gap-5 mt-10">
+                        <a target="_blank" class="font-bold text-white uppercase" href="https://codigoconjuan.com/" rel="nofollow noopener noreferrer" >Visita mi sitio</a>
+                        <a target="_blank" class="font-bold text-white uppercase" href="https://twitter.com/codigoconjuan" rel="nofollow noopener noreferrer" >Sígueme en Twitter</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="bg-blue-600 py-1"></div>
+        <div class="mx-auto max-w-7xl p-16 bg-white shadow-lg mt-10 rounded-lg text-2xl font-mono">
+        
