6a800d3b53c95a1be701fd3ae66644993f750a26
colidom <colidom@outlook.com>
2023-05-28 02:29:08 +0100

    Pre, var_dump y print_r

--- Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/07_pre.php
+++ Desarrollo Profesional de Temas y Plugins de WordPress/IntroduccionPHP/07_pre.php
@@ -1,3 +1,22 @@
-<?php 
-  include_once './includes/header.php'; 
+<?php
+include_once './includes/header.php';
 ?>
+
+<?php
+$frutas = array('manzana', 'naranja', 'melón', 'uvas');
+$estudiante = array(
+    'nombre' => 'Carlos',
+    'apellido' => 'Oliva',
+    'username' => 'colidom',
+    'pais' => 'España',
+    'edad' => 32,
+    'sexo' => 'M'
+);
+
+echo "<pre>";
+var_dump($frutas);
+echo "</pre>";
+
+echo "<pre>";
+print_r($estudiante);
+echo "</pre>";
