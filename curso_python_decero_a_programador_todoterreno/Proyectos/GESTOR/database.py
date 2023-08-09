a0782e3d166b36a8deacbb42dcc22bd4383debaf
colidom <colidom@outlook.com>
2023-08-10 00:03:29 +0100

    Añadidas clases Customer y Customers con costructor y métodos

---
+++ curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/database.py
@@ -0,0 +1,37 @@
+class Customer:
+    def __init__(self, dni, name, surname) -> None:
+        self.dni = dni
+        self.name = name
+        self.surname = surname
+
+    def __str__(self) -> str:
+        return f"({self.dni}) {self.name} {self.surname}"
+
+
+class Customers:
+    customers_list: list = []
+
+    @staticmethod
+    def find(dni):
+        for customer in Customers.customers_list:
+            if customer.dni == dni:
+                return customer
+
+    @staticmethod
+    def create(dni, name, surname):
+        customer = Customer(dni, name, surname)
+        Customers.customers_list.append(customer)
+
+    @staticmethod
+    def update(dni, name, surname):
+        for index, customer in enumerate(Customers.customers_list):
+            if customer.dni == dni:
+                Customers.customers_list[index].name = name
+                Customers.customers_list[index].surname = surname
+                return Customers.customers_list[index]
+
+    @staticmethod
+    def delete(dni):
+        for index, customer in enumerate(Customers.customers_list):
+            if customer.dni == dni:
+                return Customers.customers_list.pop[index]
