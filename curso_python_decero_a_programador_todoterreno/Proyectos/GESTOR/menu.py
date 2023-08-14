ae4bfc4bfe75d9de759b5672ffdece0c9599db42
colidom <colidom@outlook.com>
2023-08-14 23:46:39 +0100

    Implementación del menú

--- curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
+++ curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
@@ -1,5 +1,6 @@
 import os
 import helpers
+import database as db
 
 
 def launch():
@@ -23,16 +24,49 @@ def launch():
         match opcion:
             case "1":
                 print("Listing customers...\n")
+                for customer in db.Customers.customers_list:
+                    print(customer)
             case "2":
                 print("Looking for a customer...\n")
+                dni = helpers.read_text(3, 3, "DNI (2 int 1 char)").upper()
+                customer = db.Customers.find(dni)
+                print(customer) if customer else print("Customer not found")
+                break
             case "3":
                 print("Adding a customer...\n")
+                dni = helpers.read_text(3, 3, "DNI (2 int 1 char)").upper()
+                name = helpers.read_text(2, 30, "Name (2 int 30 char)").capitalize()
+                surname = helpers.read_text(
+                    2, 30, "Surname (2 int 30 char)"
+                ).capitalize()
+                db.Customers.create(dni, name, surname)
+                print("Customer successfully added")
+                break
             case "4":
                 print("Modifying a customer...\n")
+                dni = helpers.read_text(3, 3, "DNI (2 int 1 char)").upper()
+                customer = db.Customers.find(dni)
+                if customer:
+                    name = helpers.read_text(
+                        2, 30, f"Name (2 int 30 char) [{customer.name}]"
+                    ).capitalize()
+                    surname = helpers.read_text(
+                        2, 30, f"Surname (2 int 30 char)[{customer.surname}]"
+                    ).capitalize()
+                    db.Customers.update(dni, name, surname)
+                    print("Customer successfully modified")
+                print("Customer not found")
+                break
             case "5":
                 print("Deleting a customer...\n")
+                dni = helpers.read_text(3, 3, "DNI (2 int 1 char)").upper()
+                print("Customer successfully deleted") if db.Customers.delete(
+                    dni
+                ) else print("Customer not found")
+                break
             case "6":
                 print("Leaving...\n")
+                break
             case _:
                 print("Please choose one of the options above...\n")
 
