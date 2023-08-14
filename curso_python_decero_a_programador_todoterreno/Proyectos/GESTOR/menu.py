2a948ca49f3a02ba07ddc61ee5de91d16e6afcd5
colidom <colidom@outlook.com>
2023-08-15 00:10:05 +0100

    Cambio constante

--- curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
+++ curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
@@ -5,7 +5,7 @@ import database as db
 
 def launch():
     CUSTOMER_NOT_FOUND = "Customer not found❌"
-    DNI_INFO = "DNI (8 int 1 char)"
+    DNI_LENGTH = "DNI (8 int 1 char)"
 
     while True:
         helpers.clean_screen()
@@ -31,12 +31,12 @@ def launch():
                     print(customer)
             case "2":
                 print("Looking for a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_INFO).upper()
+                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
                 customer = db.Customers.find(dni)
                 print(customer) if customer else print(CUSTOMER_NOT_FOUND)
             case "3":
                 print("Adding a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_INFO).upper()
+                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
                 name = helpers.read_text(2, 30, "Name (2 int 30 char)").capitalize()
                 surname = helpers.read_text(
                     2, 30, "Surname (2 int 30 char)"
@@ -45,7 +45,7 @@ def launch():
                 print("Customer successfully added ✅")
             case "4":
                 print("Modifying a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_INFO).upper()
+                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
                 customer = db.Customers.find(dni)
                 if customer:
                     name = helpers.read_text(
