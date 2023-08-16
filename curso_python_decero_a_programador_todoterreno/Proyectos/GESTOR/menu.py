5036be8c9f732d6831377bd3f637a5b36485aeb6
colidom <colidom@outlook.com>
2023-08-16 01:47:44 +0100

    Reducida complejidad cognitiva

--- curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
+++ curso_python_decero_a_programador_todoterreno/Proyectos/GESTOR/menu.py
@@ -1,68 +1,78 @@
-import os
-import helpers
-import database as db
+import helpers, database as db
+
+CUSTOMER_NOT_FOUND = "Customer not found❌"
+DNI_LENGTH = "DNI (8 int 1 char)"
 
 
 def launch():
-    CUSTOMER_NOT_FOUND = "Customer not found❌"
-    DNI_LENGTH = "DNI (8 int 1 char)"
-
     while True:
         helpers.clean_screen()
         helpers.show_menu()
 
         opcion = input("> ")
         helpers.clean_screen()
-        customers_list = db.Customers.customers_list
 
         match opcion:
-            case "1":
-                print("Listing customers...\n")
-                for customer in customers_list:
-                    print(customer)
-            case "2":
-                print("Looking for a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
-                customer = db.Customers.find(dni)
-                print(customer) if customer else print(CUSTOMER_NOT_FOUND)
-            case "3":
-                print("Adding a customer...\n")
-
-                dni = None
-                while True:
-                    dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
-                    if helpers.validate_dni(dni, customers_list):
-                        break
-                name = helpers.read_text(2, 30, "Name (2 int 30 char)").capitalize()
-                surname = helpers.read_text(
-                    2, 30, "Surname (2 int 30 char)"
-                ).capitalize()
-                db.Customers.create(dni, name, surname)
-                print("Customer successfully added ✅")
-            case "4":
-                print("Modifying a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
-                customer = db.Customers.find(dni)
-
-                if not customer:
-                    print(CUSTOMER_NOT_FOUND)
-                    return
-
-                name = helpers.read_text(2, 30, f"New name for {customer.name}: ").capitalize()
-                surname = helpers.read_text( 2, 30, f"New surname for {customer.surname}: ").capitalize()
-                db.Customers.update(dni, name, surname)
-                print("Customer modified ✅")
-
-            case "5":
-                print("Deleting a customer...\n")
-                dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
-                print("Customer successfully deleted ✅") if db.Customers.delete(
-                    dni
-                ) else print(CUSTOMER_NOT_FOUND)
             case "6":
                 print("Leaving...\n")
                 break
+            case "1":
+                print("Listing customers...\n", *db.Customers.customers_list, sep="\n")
+            case "2":
+                find_customer()
+            case "3":
+                add_customer()
+            case "4":
+                modify_customer()
+            case "5":
+                delete_customer()
             case _:
-                print("Please choose one of the options above...\n")
-
+                print("Please choose one of the menu options...\n")
         input("\nPress ENTER to continue...")
+
+
+def find_customer():
+    print("Looking for a customer...\n")
+    dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
+    customer = db.Customers.find(dni)
+    print(customer) if customer else print(CUSTOMER_NOT_FOUND)
+
+
+def add_customer():
+    print("Adding a customer...\n")
+    dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
+    if helpers.validate_dni(dni, db.Customers.customers_list):
+        name = helpers.read_text(2, 30, "Name (2 int 30 char)").capitalize()
+        surname = helpers.read_text(2, 30, "Surname (2 int 30 char)").capitalize()
+        db.Customers.create(dni, name, surname)
+        print("Customer added ✅")
+
+
+def modify_customer():
+    print("Modifying a customer...\n")
+    dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
+    customer = db.Customers.find(dni)
+    if customer:
+        modify_existing_customer(customer)
+    else:
+        print(CUSTOMER_NOT_FOUND)
+
+
+def modify_existing_customer(customer):
+    name = helpers.read_text(2, 30, f"New name for {customer.name}: ").capitalize()
+    surname = helpers.read_text(
+        2, 30, f"New surname for {customer.surname}: "
+    ).capitalize()
+    db.Customers.update(customer.dni, name, surname)
+    print("Customer modified ✅")
+
+
+def delete_customer():
+    print("Deleting a customer...\n")
+    dni = helpers.read_text(9, 9, DNI_LENGTH).upper()
+    print("Customer successfully deleted ✅") if db.Customers.delete(dni) else print(
+        CUSTOMER_NOT_FOUND
+    )
+
+
+launch()
