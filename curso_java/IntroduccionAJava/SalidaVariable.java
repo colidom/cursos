9204867bcfc460572577dacfd7d51f6060f4f660
Carlos Oliva <colidom@outlook.com>
2023-09-20 13:02:37 +0200

    Ejercicio de salida por pantalla en Java

---
+++ curso_java/IntroduccionAJava/SalidaVariable.java
@@ -0,0 +1,11 @@
+package IntroduccionAJava;
+
+public class SalidaVariable {
+    public static void main(String args[]) {
+        int n1 = 20;
+        int n2 = 10;
+        System.out.println("El resultado es " + n1 + n2); // 2010
+        System.out.println("El resultado es " + (n1 + n2)); // 30
+    }
+
+}
