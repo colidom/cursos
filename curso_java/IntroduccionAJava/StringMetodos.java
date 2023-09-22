0548da2b7fc85196375fe13ff4be69603ddba211
Carlos Oliva <colidom@outlook.com>
2023-09-22 14:51:46 +0200

    Añadidos método trim y substring

--- curso_java/IntroduccionAJava/StringMetodos.java
+++ curso_java/IntroduccionAJava/StringMetodos.java
@@ -3,11 +3,13 @@ package IntroduccionAJava;
 public class StringMetodos {
     public static void main(String[] args) {
         String cadena = "Curso JAVA";
+        String cadena2 = "         Curso JAVA";
         int longitud = cadena.length();
         char primeraLetra = cadena.charAt(0);
         char ultimaLetra = cadena.charAt(cadena.length() - 1);
         String cadenaMayuscula = cadena.toUpperCase();
         String cadenaMinuscula = cadena.toLowerCase();
+        String palabraSubstring = cadena.substring(0, 3);
 
         // length()
         System.out.println(cadena + " tiene " + longitud + " caracteres.");
@@ -17,6 +19,8 @@ public class StringMetodos {
         // toUpperCase() toLowerCase()
         System.out.println(cadena + " en mayúsculas " + cadenaMayuscula);
         System.out.println(cadena + " en minúsculas " + cadenaMinuscula);
+        System.out.println(palabraSubstring);
+        System.out.println(cadena2.trim());
 
     }
 }
