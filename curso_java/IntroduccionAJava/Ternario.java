f54862aa0fee21214f75b5d4286f81b867a94ac6
Carlos Oliva <colidom@outlook.com>
2023-09-26 07:57:21 +0200

    Ternario

--- curso_java/IntroduccionAJava/Ternario.java
+++ curso_java/IntroduccionAJava/Ternario.java
@@ -2,6 +2,10 @@ package IntroduccionAJava;
 
 public class Ternario {
     public static void main(String[] args) {
-        
+
+        int a = 10;
+        int b = 11;
+        int x = a == 10 ? b * 2 : a;
+        System.out.println(x);
     }
 }
