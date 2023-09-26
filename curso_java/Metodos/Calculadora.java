501bd06577818288aa5e0f86e15511a5023c43e4
Carlos Oliva <colidom@outlook.com>
2023-09-26 14:18:37 +0200

    Método suma

---
+++ curso_java/Metodos/Calculadora.java
@@ -0,0 +1,13 @@
+package Metodos;
+
+public class Calculadora {
+
+    static int suma(int a, int b) {
+        int resultado = a + b;
+        return resultado;
+    }
+
+    int valor1 = 1;
+    int valor2 = 2;
+    int c = suma(valor1, valor2);
+}
