0aa2baab04c08ba2017974967547732aa83b1d47
Carlos Oliva <colidom@outlook.com>
2023-09-28 14:38:21 +0200

    Tipos de datos primitivos

---
+++ java_desde_0/TiposDatos/src/variables/EjemploTiposDato.java
@@ -0,0 +1,28 @@
+package variables;
+
+public class EjemploTiposDato {
+
+	public static void main(String[] args) {
+		
+		// Números enteros
+		byte b = 8;
+		short pequeño = 123;
+		int entero = 123456789;
+		long grande = 2239998988223L;
+		long otroGrande = 123_456_789_123_456_789L;
+		
+		// Números decimales
+		float decimalPequeño = 123.456f;
+		double decimalGrande = 234023948324092.2121212;
+		double notacionCientifica = 1.1234132E-10;
+		
+		// Variables booleanas
+		boolean verdadero = true;
+		boolean falso = false;
+		
+		// Caracteres y cadenas
+		char c = 'c';
+		String mensaje = "Hola Mundo";
+	}
+
+}
