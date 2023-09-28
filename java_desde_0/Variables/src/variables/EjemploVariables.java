f8bb6f5bcb713a69a4c03ca63021420c3ff1a576
Carlos Oliva <colidom@outlook.com>
2023-09-28 13:38:37 +0200

    variables

---
+++ java_desde_0/Variables/src/variables/EjemploVariables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class EjemploVariables {
+
+	public static void main(String[] args) {
+		String msg = "Hola mundo";
+		int x;
+		x = 7;
+		msg = "Hasta luego mundo";
+	}
+
+}
