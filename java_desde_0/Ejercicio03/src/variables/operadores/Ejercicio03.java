7c0600203c2cb0e011566cc8f1a0d8b176e6e812
Carlos Oliva <colidom@outlook.com>
2023-09-29 14:06:47 +0200

    Resultado ejercicio3

---
+++ java_desde_0/Ejercicio03/src/variables/operadores/Ejercicio03.java
@@ -0,0 +1,20 @@
+package variables.operadores;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+
+		// Calcular area del rectángulo
+		double ancho = 5;
+		double altura = 10;
+
+		double area = ancho * altura;
+
+		// Calcular el perímetro del rectángulo
+		double perimetro = 2 * altura + 2 * ancho;
+
+		System.out.println("El area del rectángulo es:" + area);
+		System.out.println("El perímetro del rectángulo es:" + perimetro);
+	}
+
+}
