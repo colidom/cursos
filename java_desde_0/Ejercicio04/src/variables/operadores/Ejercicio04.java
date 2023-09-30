31c00caf71ae533d7bacefb2789fd8ee6c52d340
Carlos Oliva <colidom@outlook.com>
2023-09-30 23:29:16 +0200

    Solución ejercicio

--- java_desde_0/Ejercicio04/src/variables/operadores/Ejercicio04.java
+++ java_desde_0/Ejercicio04/src/variables/operadores/Ejercicio04.java
@@ -3,7 +3,18 @@ package variables.operadores;
 public class Ejercicio04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double radio = 5.00; // Radio del círculo
+
+		// Cálculo del perímetro del círculo
+		double perimetro = 2 * Math.PI * radio;
+
+		// Cálculo del área del círculo
+		double area = Math.PI * radio * radio;
+
+		// Mostramos resultados por consola
+		System.out.println("El perímetro del círculo es: " + perimetro);
+		System.out.println("El área del círculo es: " + area);
 
 	}
 
