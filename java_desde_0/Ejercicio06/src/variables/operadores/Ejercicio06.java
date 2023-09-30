fc6ebf68a8b494b363ea0624481108490b23607d
Carlos Oliva <colidom@outlook.com>
2023-09-30 23:45:42 +0200

    Ejercicio con condición ternaria

--- java_desde_0/Ejercicio06/src/variables/operadores/Ejercicio06.java
+++ java_desde_0/Ejercicio06/src/variables/operadores/Ejercicio06.java
@@ -10,11 +10,16 @@ public class Ejercicio06 {
 		System.out.print("Introduzca un número: ");
 		double numero = sc.nextDouble();
 
-		if (numero % 2 == 0) {
-			System.out.println("El número es par");
-		} else {
-			System.out.println("El número es impar");
-		}
+		/*
+		 * if (numero % 2 == 0) {
+		 * System.out.println("El número es par");
+		 * } else {
+		 * System.out.println("El número es impar");
+		 * }
+		 */
+
+		String resultado = (numero % 2 == 0) ? "par" : "impar";
+		System.out.println("El número " + numero + " es " + resultado);
 
 	}
 
