15931ae3829ff82dab328347fd5612110a64c4e2
Carlos Oliva <colidom@outlook.com>
2023-10-01 00:12:32 +0200

    Solución del ejercicio 10, asumiendo que quiere que calcule el tiempo, ejercicio ambiguo

--- java_desde_0/Ejercicio10/src/variables/operadores/Ejercicio10.java
+++ java_desde_0/Ejercicio10/src/variables/operadores/Ejercicio10.java
@@ -3,8 +3,17 @@ package variables.operadores;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Datos del problema
+		double velocidad = 17.0; // Velocidad en km por segundo
+		double espacioInicial = 150000.0; // Espacio inicial en km
 
+		// Calculamos el tiempo
+		double tiempo = espacioInicial / velocidad;
+
+		// Mostramos los resultados
+		System.out.println("Velocidad del objeto: " + velocidad + " km/s");
+		System.out.println("Espacio inicial: " + espacioInicial + " km");
+		System.out.println("Tiempo transcurrido: " + tiempo + " segundos");
 	}
 
 }
