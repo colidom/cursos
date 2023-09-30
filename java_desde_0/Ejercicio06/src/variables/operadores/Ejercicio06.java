7c42b30ab43d01f8155bb8bc05ab4173effbc592
Carlos Oliva <colidom@outlook.com>
2023-09-30 23:41:31 +0200

    Añadida solución

---
+++ java_desde_0/Ejercicio06/src/variables/operadores/Ejercicio06.java
@@ -0,0 +1,21 @@
+package variables.operadores;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Introduzca un número: ");
+		double numero = sc.nextDouble();
+
+		if (numero % 2 == 0) {
+			System.out.println("El número es par");
+		} else {
+			System.out.println("El número es impar");
+		}
+
+	}
+
+}
