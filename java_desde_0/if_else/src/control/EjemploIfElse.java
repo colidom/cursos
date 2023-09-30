37269d07ca30bf5e6d605a0a6968077384ca286b
Carlos Oliva <colidom@outlook.com>
2023-10-01 01:12:50 +0200

    if else

---
+++ java_desde_0/if_else/src/control/EjemploIfElse.java
@@ -0,0 +1,16 @@
+package control;
+
+public class EjemploIfElse {
+
+	public static void main(String[] args) {
+		int nota = 10;
+
+		if (nota > 5) {
+			System.out.println("Aprobado");
+		} else {
+			System.out.println("Suspendido");
+		}
+
+	}
+
+}
