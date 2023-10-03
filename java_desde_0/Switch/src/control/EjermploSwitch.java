597982f9be1ad81e58a65fee020a01e1ff871a0c
Carlos Oliva <colidom@outlook.com>
2023-10-03 14:08:08 +0200

    Ejemplo de Switch

--- java_desde_0/Switch/src/control/EjermploSwitch.java
+++ java_desde_0/Switch/src/control/EjermploSwitch.java
@@ -3,7 +3,34 @@ package control;
 public class EjermploSwitch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nota = 9;
+
+		switch (nota) {
+			case 10:
+			case 9:
+				System.out.println("Sobresaliente");
+				break;
+			case 8:
+			case 7:
+				System.out.println("Notable");
+				break;
+			case 6:
+				System.out.println("Bien");
+				break;
+			case 5:
+				System.out.println("Suficiente");
+				break;
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+				System.out.println("Insuficiente");
+				break;
+			default:
+				System.out.println("Calificación inválida");
+				break;
+		}
 
 	}
 
