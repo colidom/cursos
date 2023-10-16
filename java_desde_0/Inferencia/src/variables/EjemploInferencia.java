e9e61b5577b161c7bd7c8a0d38cb8a5dcae280d0
Carlos Oliva <colidom@outlook.com>
2023-10-16 13:04:28 +0200

    Correcciones varias

--- java_desde_0/Inferencia/src/variables/EjemploInferencia.java
+++ java_desde_0/Inferencia/src/variables/EjemploInferencia.java
@@ -3,12 +3,14 @@ package variables;
 public class EjemploInferencia {
 
 	public static void main(String[] args) {
-		
+
 		// var uno; // error
 		var cadena = "Hola Mundo";
 		var x = 7;
 		var decimal = 1234.456;
-		
+
+		System.out.println(cadena + " " + x + " " + decimal);
+
 	}
 
 }
