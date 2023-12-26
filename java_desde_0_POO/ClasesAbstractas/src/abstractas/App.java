ab0814dfa7fb9962fb0f0b2d43a307fa3e0dd534
colidom <colidom@outlook.com>
2023-12-27 00:31:49 +0100

    Formateo clase main principal

--- java_desde_0_POO/ClasesAbstractas/src/abstractas/App.java
+++ java_desde_0_POO/ClasesAbstractas/src/abstractas/App.java
@@ -6,12 +6,12 @@ public class App {
 
 		// Genera error porque la clase es abstracta
 		// Vehiculo v = new Vehiculo("Marca", "Modelo");
-		
+
 		Coche c = new Coche("Hyundai", "Coupe", 180);
 		c.mostrarInformacion();
 		c.mostrarVelocidadMaxima();
 		c.conducir();
-		
+
 		VehiculoAereo h = new Helicoptero("Boeing", "CH-47 Chinook", 7315);
 		h.mostrarInformacion();
 		h.despegar();
