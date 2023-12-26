cd647014b648ef6594f0a12b53eafc0c8f1ed46f
colidom <colidom@outlook.com>
2023-12-27 00:30:24 +0100

    Formateo

--- java_desde_0_POO/ClasesAbstractas/src/abstractas/VehiculoAereo.java
+++ java_desde_0_POO/ClasesAbstractas/src/abstractas/VehiculoAereo.java
@@ -1,7 +1,7 @@
 package abstractas;
 
 public abstract class VehiculoAereo extends Vehiculo {
-	
+
 	private int alturaMaxima;
 
 	public VehiculoAereo(String marca, String modelo, int alturaMaxima) {
@@ -12,18 +12,17 @@ public abstract class VehiculoAereo extends Vehiculo {
 	@Override
 	public void conducir() {
 		System.out.println("Realmente se llama pilotar...");
-		
+
 	}
-	
+
 	@Override
 	public void mostrarInformacion() {
 		super.mostrarInformacion();
-		System.out.println("La altura máxima: " + this.alturaMaxima);
+		System.out.println("La altura mï¿½xima: " + this.alturaMaxima);
 	}
 
 	public abstract void despegar();
-	
+
 	public abstract void aterrizar();
 
-	
 }
