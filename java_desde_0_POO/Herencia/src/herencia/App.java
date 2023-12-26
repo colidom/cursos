cdcd942439a9135a8232c54c9a8c5fbb642b4d38
colidom <colidom@outlook.com>
2023-12-26 22:52:31 +0100

    Formateo de código

--- java_desde_0_POO/Herencia/src/herencia/App.java
+++ java_desde_0_POO/Herencia/src/herencia/App.java
@@ -3,16 +3,16 @@ package herencia;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		Perro p = new Perro(60, 28, "Labrador");
-		Gato g = new Gato(45, 6, "Montés");
-		
+		Gato g = new Gato(45, 6, "MontÃ©s");
+
 		System.out.println(p);
 		System.out.println(g);
-		
+
 		p.ladrar();
 		g.maullar();
-		p.saludar(); // Sobrecarga del método
+		p.saludar(); // Sobrecarga del metodo
 		g.saludar();
 	}
 
