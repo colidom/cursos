d97d7a084d929324a4904596a06e2cc03e75dbc4
colidom <colidom@outlook.com>
2023-12-27 00:32:36 +0100

    Formateo clase Perro

--- java_desde_0_POO/Herencia/src/herencia/Perro.java
+++ java_desde_0_POO/Herencia/src/herencia/Perro.java
@@ -5,13 +5,13 @@ public class Perro extends Animal {
 	public Perro(int estatura, double peso, String raza) {
 		super(estatura, peso, raza);
 	}
-	
+
 	public void ladrar() {
 		System.out.println("Guau guay guau...");
 	}
-	
+
 	public void saludar() {
-		System.out.println("Hola soy un perro de raza %s y peso %.2f kilos" .formatted(this.raza, this.peso));
+		System.out.println("Hola soy un perro de raza %s y peso %.2f kilos".formatted(this.raza, this.peso));
 	}
-	
+
 }
