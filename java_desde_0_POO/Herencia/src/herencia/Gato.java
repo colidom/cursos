823c7aa7866adbac8d42f71fb5ae307168827a44
colidom <colidom@outlook.com>
2023-12-27 00:32:26 +0100

    Formateo clase Gato

--- java_desde_0_POO/Herencia/src/herencia/Gato.java
+++ java_desde_0_POO/Herencia/src/herencia/Gato.java
@@ -5,7 +5,7 @@ public class Gato extends Animal {
 	public Gato(int estatura, double peso, String raza) {
 		super(estatura, peso, raza);
 	}
-	
+
 	public void maullar() {
 		System.out.println("Miau miau miau...");
 	}
