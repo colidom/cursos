1294a7df5778a457aab41244cd4ab1cc18697559
colidom <colidom@outlook.com>
2024-01-30 23:50:10 +0100

    Añadidos materiales del curso

---
+++ Laravel - Introduccion POO/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programación Orientada a Objetos PHP  - Código Con Juan</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css">
+</head>
+<body class="bg-gray-50">
+    <div class="mt-10 max-w-2xl mx-auto text-center space-y-3">
+        <h1 class="text-4xl font-extrabold text-indigo-600 mb-10" >
+            Programación Orientada a Objetos PHP  - Código Con Juan
+        </h1>
+        <a class="block" href="http://www.codigoconjuan.com" target="_blank">Sitio Web: codigoconjuan.com</a>
+        <a class="block" href="https://twitter.com/codigoconjuan" target="_blank">Twitter: @codigoconjuan</a>
+        <p class="text-2xl text-indigo-600 block">#codigoConJuan</p>
+    </div>
+
+    <div class="mt-10 max-w-2xl mx-auto shadow bg-white p-10 text-base">
