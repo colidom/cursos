d6679356cad7391bfb6985e1d1e1ebebd769ebb1
colidom <colidom@outlook.com>
2024-01-31 00:16:26 +0100

    Constructor Property Promotion

--- Laravel - Introduccion POO/06.php
+++ Laravel - Introduccion POO/06.php
@@ -1,2 +1,25 @@
 <?php
 include 'includes/header.php';
+
+// Constructor property promotion
+class Empleado
+{
+
+    public function __construct(
+        public $nombre,
+        public $apellido,
+        public $departamento,
+        public $email,
+        public $codigo,
+    ) {
+
+    }
+}
+
+$empleado1 = new Empleado("Carlos", "Oliva", "IT", "colidom@outlook.com", 006);
+$empleado2 = new Empleado("Javier", "Oliva", "IT", "jolidom@outlook.com", 007);
+
+echo "<pre>";
+var_dump($empleado1);
+var_dump($empleado2);
+echo "</pre>";
