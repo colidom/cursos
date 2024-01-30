79d2a1a2514ae948194c1782ecce0d9297f6f4c3
colidom <colidom@outlook.com>
2024-01-30 23:57:33 +0100

    Instanciar una clase

--- Laravel - Introduccion POO/02.php
+++ Laravel - Introduccion POO/02.php
@@ -1,2 +1,17 @@
 <?php
 include 'includes/header.php';
+
+class Empleado
+{
+
+}
+
+$empleado1 = new Empleado;
+$empleado2 = new Empleado;
+$empleado3 = new Empleado;
+
+echo "<pre>";
+var_dump($empleado1);
+var_dump($empleado2);
+var_dump($empleado3);
+echo "</pre>";
