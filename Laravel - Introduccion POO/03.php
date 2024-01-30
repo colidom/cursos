bd2c73c88ad25921826e9ccea56b61ed3990e9a4
colidom <colidom@outlook.com>
2024-01-31 00:02:21 +0100

    Atributos de una clase

--- Laravel - Introduccion POO/03.php
+++ Laravel - Introduccion POO/03.php
@@ -1,2 +1,22 @@
 <?php
 include 'includes/header.php';
+
+class Empleado
+{
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+}
+
+$empleado1 = new Empleado;
+$empleado1->nombre = "Carlos";
+$empleado1->apellido = "Oliva";
+$empleado1->departamento = "IT";
+$empleado1->email = "colidom@outlook.com";
+$empleado1->codigo = "C06";
+
+echo "<pre>";
+var_dump($empleado1);
+echo "</pre>";
