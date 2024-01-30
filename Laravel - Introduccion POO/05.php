194dcd66551b4a7f8e5b801c8e2d167ebeb77e11
colidom <colidom@outlook.com>
2024-01-31 00:14:37 +0100

    Tipado en php

--- Laravel - Introduccion POO/05.php
+++ Laravel - Introduccion POO/05.php
@@ -1,2 +1,28 @@
 <?php
 include 'includes/header.php';
+
+class Empleado
+{
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+
+    public function __construct(string $nombre, string $apellido,
+        string $departamento, string $email, int $codigo) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->departamento = $departamento;
+        $this->email = $email;
+        $this->codigo = $codigo;
+    }
+}
+
+$empleado1 = new Empleado("Carlos", "Oliva", "IT", "colidom@outlook.com", 006);
+$empleado2 = new Empleado("Javier", "Oliva", "IT", "jolidom@outlook.com", 007);
+
+echo "<pre>";
+var_dump($empleado1);
+var_dump($empleado2);
+echo "</pre>";
