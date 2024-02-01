2e0a9098e8c7faec5ec6e5b7934dea4dd7dc9138
colidom <colidom@outlook.com>
2024-02-01 01:40:56 +0100

    Comunicar dos clases

--- Laravel - Introduccion POO/13.php
+++ Laravel - Introduccion POO/13.php
@@ -1,2 +1,48 @@
 <?php
 include 'includes/header.php';
+include 'DB.php';
+
+// Comunicar dos Clases
+class Empleado
+{
+
+    public function __construct(
+        protected $nombre,
+        public $apellido,
+        public $departamento,
+        public $email,
+        public $codigo,
+    ) {}
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        return $this->nombre = $nombre;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido)
+    {
+        return $this->apellido = $apellido;
+    }
+
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+}
+
+$empleado1 = new Empleado("Carlos", "Oliva", "IT", "colidom@outlook.com", 006);
+$nombre = $empleado1->getNombre();
+
+$db = new DB($nombre);
+var_dump($db);
+$db->guardar();
--- Laravel - Introduccion POO/DB.php
+++ Laravel - Introduccion POO/DB.php
@@ -1,2 +1,15 @@
 <?php
-include 'includes/header.php';
+
+class DB {
+
+    protected $nombre;
+
+    public function __construct($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function guardar() {
+        echo "Almacenando... $this->nombre";
+    }
+}
