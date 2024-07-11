e9f325d993d05ddf82adebb4647a177fed40c72a
colidom <colidom@outlook.com>
2024-07-11 23:57:44 +0200

    Ejemplo de clase

--- php_for_beginners/Dynamic web applications/index.php
+++ php_for_beginners/Dynamic web applications/index.php
@@ -1,5 +1,19 @@
 <?php
 
 require 'functions.php';
-
 require 'router.php';
+
+// Connection to MySQL database
+class Person
+{
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+    }
+}
+
+$person = new Person();
+$person->name = 'John Doe';
+$person->age = 30;
