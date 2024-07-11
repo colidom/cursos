0b372452e62b85d1a786e92521054a75f6f90631
colidom <colidom@outlook.com>
2024-07-12 00:16:06 +0200

    PDO First steps

--- php_for_beginners/Dynamic web applications/index.php
+++ php_for_beginners/Dynamic web applications/index.php
@@ -4,19 +4,13 @@ require 'functions.php';
 //require 'router.php';
 
 // Connection to MySQL database
-class Person
-{
-    public $name;
-    public $age;
+$dsn = "mysql:host=localhost;port=3306;user=colidom;password=Mysql2023;dbname=PHPForBeginners;charset=utf8mb4";
 
-    public function breathe()
-    {
-        echo $this->name . " is breathing...";
-    }
-}
+// Asignar la instancia de PDO a la variable $pdo
+$pdo = new PDO($dsn);
 
-$person = new Person();
-$person->name = 'John Doe';
-$person->age = 30;
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
 
-dd($person->breathe());
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
