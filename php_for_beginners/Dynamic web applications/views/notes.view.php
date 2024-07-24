603394096e3e50dffcfb6023d00dc63a52d6259e
colidom <colidom@outlook.com>
2024-07-24 23:33:47 +0100

    Formateo

--- php_for_beginners/Dynamic web applications/views/notes.view.php
+++ php_for_beginners/Dynamic web applications/views/notes.view.php
@@ -4,9 +4,12 @@
 <?php require('partials/banner.php'); ?>
 <?php require('partials/main.php'); ?>
 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <?php foreach ($notes as $note) : ?>
-        <li><a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= $note['title']; ?></a></li>
-    <?php endforeach; ?>
+    <ul><?php foreach ($notes as $note) : ?>
+            <li>
+                <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= $note['title']; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
 
 <?php require('partials/footer.php'); ?>
