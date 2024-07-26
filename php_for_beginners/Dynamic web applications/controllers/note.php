d3a56b607a9cf2a462887d54261205c727e346ed
colidom <colidom@outlook.com>
2024-07-26 22:45:05 +0100

    Refactorizando  consulta de notas

--- php_for_beginners/Dynamic web applications/controllers/note.php
+++ php_for_beginners/Dynamic web applications/controllers/note.php
@@ -5,8 +5,15 @@ $heading = "My Note";
 $config = require 'config.php';
 $db = new Database($config['database'], $config['credentials']['username'], $config['credentials']['password']);
 
-$query = "select * from notes where id = ?";
-$note = $db->query($query, [$_GET['id']])->findOrFail();
+$id = $_GET['id'] ?? null;
+
+if (!$id) {
+    require 'views/404.php';
+}
+
+$note = $db->query("SELECT * FROM notes WHERE id = :id", [
+    ':id' => $id
+])->findOrFail();
 
 $currentCuserId = 1;
 
