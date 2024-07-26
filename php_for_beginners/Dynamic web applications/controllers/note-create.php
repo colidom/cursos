37114e059147f2f41b58984ee3255a2fb771e7ea
colidom <colidom@outlook.com>
2024-07-26 22:52:52 +0100

    Añadida redirección a notas al crear una nota

--- php_for_beginners/Dynamic web applications/controllers/note-create.php
+++ php_for_beginners/Dynamic web applications/controllers/note-create.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ':user_id' => $user_id
         ]);
 
-        echo "Note created successfully!";
+        header('Location: /notes');
     } else {
         echo "Title, body, and user ID are required!";
     }
