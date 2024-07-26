f9baf657d26f58ee038aef5d1d02e3c01e842ab8
colidom <colidom@outlook.com>
2024-07-26 22:37:21 +0100

    Añadido campo título

--- php_for_beginners/Dynamic web applications/views/note-create.view.php
+++ php_for_beginners/Dynamic web applications/views/note-create.view.php
@@ -7,6 +7,14 @@
 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <form method="POST">
         <div class="space-y-12">
+            <div class="sm:col-span-4">
+                <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                <div class="mt-2">
+                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-900 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        <input type="text" name="title" id="title" autocomplete="title" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="">
+                    </div>
+                </div>
+            </div>
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="col-span-full">
