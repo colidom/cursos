540e767d7e719ad802a823a95edaab0dea2cea3a
colidom <colidom@outlook.com>
2024-08-16 00:29:01 +0100

    Añadido método spl_autoload_register para requerir la clase Database

--- php_for_beginners/Dynamic web applications/public/index.php
+++ php_for_beginners/Dynamic web applications/public/index.php
@@ -3,6 +3,8 @@
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'functions.php';
 
-require base_path('Database.php');
-require base_path('Response.php');
+spl_autoload_register(function ($class) {
+    require base_path($class . '.php');
+});
+
 require base_path('router.php');
