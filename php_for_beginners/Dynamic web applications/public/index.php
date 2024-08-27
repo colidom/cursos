39cdabb70ebdb5c19165131219109ea146a9a659
colidom <colidom@outlook.com>
2024-08-27 23:55:17 +0100

    Cambiada forma de uso de la clase Router

--- php_for_beginners/Dynamic web applications/public/index.php
+++ php_for_beginners/Dynamic web applications/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Router;
+
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'Core/functions.php';
 
@@ -9,7 +11,7 @@ spl_autoload_register(function ($class)
     require base_path("{$class}.php");
 });
 
-$router = new \Core\Router();
+$router = new Router();
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
