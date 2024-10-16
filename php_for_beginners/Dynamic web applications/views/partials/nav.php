d9945ed0757593cea100fd7659ce371f179069db
colidom <colidom@outlook.com>
2024-10-16 22:07:57 +0200

    Actualizado logo

--- php_for_beginners/Dynamic web applications/views/partials/nav.php
+++ php_for_beginners/Dynamic web applications/views/partials/nav.php
@@ -3,7 +3,7 @@
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
-                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                    <img class="h-8 w-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
