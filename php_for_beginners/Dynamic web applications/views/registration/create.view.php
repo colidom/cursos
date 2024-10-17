5313fc757df76fd0193d358f543bcfd1a2d4bd3b
colidom <colidom@outlook.com>
2024-10-17 22:28:03 +0200

    Corregido enlace de logo

--- php_for_beginners/Dynamic web applications/views/registration/create.view.php
+++ php_for_beginners/Dynamic web applications/views/registration/create.view.php
@@ -4,7 +4,7 @@
 <main>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
             <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register for a new account</h2>
         </div>
 
