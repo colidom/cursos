07d1dd7afa3a1a4d2c9eb46dd40f8078602a3191
colidom <colidom@outlook.com>
2024-10-29 21:38:00 +0100

    Cambios para comprobar las credenciales del usuario y hacer login

--- php_for_beginners/Dynamic web applications/controllers/sessions/store.php
+++ php_for_beginners/Dynamic web applications/controllers/sessions/store.php
@@ -1,3 +1,38 @@
 <?php
 
-dd("login");
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$db = App::resolve(Database::class);
+
+$errors = [];
+if (!Validator::email($email)) {
+    $errors['email'] = "Please provide a valid email address";
+}
+
+if (!empty($errors)){
+    return view('sessions/create.view.php', [
+        "errors" => $errors
+    ]);
+}
+
+// Match the credentials
+$user = $db->query('select * from users where email = :email', [
+    'email'=> $email
+])->find();
+
+if (!$user) {
+    view('sessions/create.view.php', [
+        "errors" => [
+            'email' => 'No matching account found for that email address!'
+        ]
+    ]);
+};
+
+login([
+    'email'=> $email
+]);
