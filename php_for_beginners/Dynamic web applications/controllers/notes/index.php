f54402591a4e2e70034b0b9c7d56dc22e477da69
colidom <colidom@outlook.com>
2024-11-29 21:58:57 +0100

    Quitada comprobación de si existen notas o no, ya que es innecesario, es un resto de código

--- php_for_beginners/Dynamic web applications/controllers/notes/index.php
+++ php_for_beginners/Dynamic web applications/controllers/notes/index.php
@@ -6,11 +6,6 @@ $db =App::resolve(Core\Database::class);
 
 $notes = $db->query("SELECT * FROM notes")->findAll();
 
-if (!$notes) {
-    // Manejo de error si no se encuentran notas
-    die('No notes found');
-}
-
 view('notes/index.view.php', [
     'heading' => "My Notes",
     'notes' => $notes
