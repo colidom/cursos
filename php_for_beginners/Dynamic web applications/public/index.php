4175519b38ff97957409f13641fa32a584ae60b2
colidom <colidom@outlook.com>
2024-12-21 20:35:57 +0000

    Añadiendo autoload al proyecto

--- php_for_beginners/Dynamic web applications/public/index.php
+++ php_for_beginners/Dynamic web applications/public/index.php
@@ -8,13 +8,7 @@ session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class)
-{
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
-
+require BASE_PATH . '/vendor/autoload.php';
 require base_path('bootstrap.php');
 
 $router = new Router();
