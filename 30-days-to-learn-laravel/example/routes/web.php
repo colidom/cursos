869a624c40b93f618eb2ef7e9340279ea3e0f595
colidom <colidom@outlook.com>
2025-01-21 00:38:20 +0000

    First route and view

--- 30-days-to-learn-laravel/example/routes/web.php
+++ 30-days-to-learn-laravel/example/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
