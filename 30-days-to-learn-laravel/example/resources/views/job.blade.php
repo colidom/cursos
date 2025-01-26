8f3ff559d52e2301c6abe652f03162d0d31fb7f0
colidom <colidom@outlook.com>
2025-01-26 00:27:03 +0000

    Cambiando clase

--- 30-days-to-learn-laravel/example/resources/views/job.blade.php
+++ 30-days-to-learn-laravel/example/resources/views/job.blade.php
@@ -3,5 +3,5 @@
         Job Page
     </x-slot:heading>
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p class="font-bold">This Job Pays {{ $job['salary'] }}€ per year.</p>
+    <p class="text-blue-500">This Job Pays {{ $job['salary'] }}€ per year.</p>
 </x-layout>
