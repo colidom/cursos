86e6aa10c9c9e7bd4e689d843ae31beb53a53c79
colidom <colidom@outlook.com>
2025-01-27 19:12:06 +0000

    Meet Eloquent

--- 30-days-to-learn-laravel/example/app/Models/Job.php
+++ 30-days-to-learn-laravel/example/app/Models/Job.php
@@ -2,42 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Programmer',
-                'salary' => '40000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Teacher',
-                'salary' => '40000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Director',
-                'salary' => '10000'
-            ]
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 
-    /**
-     * Find a job by its ID.
-     *
-     * @param int $id The ID of the job to find
-     * @return array The job found matching the provided ID
-     */
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404, 'Job not found.');
-        }
-        return $job;
-    }
 }
--- 30-days-to-learn-laravel/example/routes/web.php
+++ 30-days-to-learn-laravel/example/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
-    return view('home', [
+   return view('home', [
         'greeting' => 'Hello',
         'name' => 'Colidom'
         ]);
