de87fb418f03b09b352fd9a34ea8aae483bd1f6e
colidom <colidom@outlook.com>
2025-03-05 22:10:08 +0000

    Understanding Database Seeders

--- 30-days-to-learn-laravel/example/database/seeders/DatabaseSeeder.php
+++ 30-days-to-learn-laravel/example/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Jhon',
+            'last_name' => 'Doe',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
