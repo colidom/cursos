468389df3fc4d6a26b17f601aaa8ffe870143126
colidom <colidom@outlook.com>
2025-03-12 01:53:14 +0000

    Deleted ml-3 tailwind class

--- 30-days-to-learn-laravel/example/resources/views/Components/button.blade.php
+++ 30-days-to-learn-laravel/example/resources/views/Components/button.blade.php
@@ -1,4 +1,4 @@
-<a {{ $attributes->merge(['class' => 'relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium
+<a {{ $attributes->merge(['class' => 'relative inline-flex items-center px-4 py-2 text-sm font-medium
     text-white bg-gray-100 border border-gray-100 leading-5 rounded-md
     hover:text-gray-200 hover:bg-gray-700 focus:outline-none focus:ring ring-gray-200
     focus:border-blue-200 active:bg-gray-300 active:text-gray-200
