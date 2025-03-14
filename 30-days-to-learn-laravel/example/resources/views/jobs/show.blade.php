96c80d2223f62c315100bc5a693702104a83d487
colidom <colidom@outlook.com>
2025-03-14 23:33:48 +0000

    Añadido caracter flecha atras

--- 30-days-to-learn-laravel/example/resources/views/jobs/show.blade.php
+++ 30-days-to-learn-laravel/example/resources/views/jobs/show.blade.php
@@ -5,6 +5,6 @@
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
     <p class="text-blue-500">This Job Pays {{ $job['salary'] }}€ per year.</p>
     <div class="mt-6">
-        <x-button href="/jobs">Back</x-button>
+        <x-button href="/jobs">« Back</x-button>
     </div>
 </x-layout>
