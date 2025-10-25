6a04af4550fefae8df666374ba2458f6b38ea5b2
colidom <colidom@outlook.com>
2025-10-25 17:32:38 +0100

    Añadido ejercicio fastapi

---
+++ fastAPI/test.py
@@ -0,0 +1,16 @@
+def append_and_print(item, lista=[]):
+    lista.append(item)
+    print(lista)
+
+
+append_and_print(5)
+append_and_print(1)
+append_and_print([2])
+append_and_print(3, [])
+append_and_print("4", [])
+append_and_print(5)
+
+print(1, 1, 2, 2, 3, 3, 4, 4, 5, 5)
+print([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])
+print((1, 1, 2, 2, 3, 3, 4, 4, 5, 5))
+print({1, 1, 2, 2, 3, 3, 4, 4, 5, 5})
