03a9bc6f1210590bf039aba014bce5165a181c4e
colidom <colidom@outlook.com>
2025-10-25 17:32:27 +0100

    Corregido detalle pasado por alto, usando la función abs

--- microsoft-python-programming-fundamentals/Module3/temperatures_analitics.py
+++ microsoft-python-programming-fundamentals/Module3/temperatures_analitics.py
@@ -24,9 +24,10 @@ print("Highest temperature:", highest_temp, "°C")
 print("Lowest temperature:", lowest_temp, "°C")
 
 # Calculate the average temperature
-average_temp = round(sum(antarctic_temperatures) / len(antarctic_temperatures), 1)
+average_temp = sum(antarctic_temperatures) / len(antarctic_temperatures)
+average_temp = round(average_temp, 1)  # Redondear a un decimal
 print("Average temperature:", average_temp, "°C")
 
 # Find the absolute value of the coldest temperature
-coldest_temp_abs = lowest_temp * -1
-print("The coldest temperature was", coldest_temp_abs, "°C below freezing.")
+coldest_temp_abs = abs(lowest_temp)
+print("The absolute value of the coldest temperature is:", coldest_temp_abs, "°C")
