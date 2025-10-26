2099c436623d4b560872f8e47821198b71cfd638
colidom <colidom@outlook.com>
2025-10-26 01:41:33 +0100

    Añadido test de función contains_five

---
+++ microsoft-python-programming-fundamentals/Module6/test_Contains_five.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_contains_five():
+    """
+    Tests the 'contains_five' function.
+    """
+    # Example list for testing
+    my_list = [1, 3, 5, 7, 9]
+    # Write your Python assert below
+    assert contains_five(my_list) == True
