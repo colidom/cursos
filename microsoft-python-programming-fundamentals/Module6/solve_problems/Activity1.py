2aaf34498c2cc22cb370dd52ef34736dfb381c41
colidom <colidom@outlook.com>
2025-10-27 00:41:42 +0000

    Unificado todo en Atividad 1

--- microsoft-python-programming-fundamentals/Module6/solve_problems/Activity1.py
+++ microsoft-python-programming-fundamentals/Module6/solve_problems/Activity1.py
@@ -1,4 +1,8 @@
-"""Task 1: Define a Discount Function
+"""Activity 1: Making Decisions with Conditional Statements
+Introduction
+As a data analyst for an online book store, you're tasked with analyzing customer purchasing patterns. The marketing team wants to send special discount codes to customers based on the number of books they’ve purchased. If a customer has purchased enough books, they’ll receive a discount. If they’ve purchased more, they might qualify for an even better discount.
+
+Task 1: Define a Discount Function
 You need to write a function that helps the marketing team decide which customers should receive a discount.
 
 Define a function called send_discount that accepts two arguments:
@@ -26,3 +30,18 @@ def send_discount(books_purchased, discount_threshold):
 
 send_discount(3, 5)  # Should print No discount.
 send_discount(7, 5)  # Should print Discount applied!
+
+
+def send_discount(books_purchased, discount_threshold, bonus_threshold):
+    if books_purchased >= bonus_threshold:
+        print("Big discount applied!")
+    elif books_purchased >= discount_threshold:
+        print("Discount applied!")
+    else:
+        print("No discount.")
+
+
+# Checking your results
+send_discount(3, 5, 10)  # Should print No discount.
+send_discount(7, 5, 10)  # Should print Discount applied!
+send_discount(12, 5, 10)  # Should print Big discount applied!
